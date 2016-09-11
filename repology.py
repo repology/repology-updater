2b53fd954aebd7f2253e5dfbed32d90d1c7f3684
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-09-11 23:36:38 +0300

    Download indexes for FreeBSD and Debian

--- repology.py
+++ repology.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 import os
+import subprocess
 
 def Main():
+    if not os.path.isfile('freebsd.list'):
+        subprocess.check_call("wget -qO- http://www.FreeBSD.org/ports/INDEX-11.bz2 | bunzip2 > freebsd.list", shell = True)
+    if not os.path.isfile('debian.list'):
+        subprocess.check_call("wget -qO- http://ftp.debian.org/debian/dists/stable/main/source/Sources.gz | gunzip > debian.list", shell = True)
     return 0
 
 if __name__ == '__main__':
