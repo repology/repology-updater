31db0d8bfcfe5b03268d39dab17be9b47e87fc73
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-09-12 18:58:57 +0300

    Add support for Gentoo portage

--- repology.py
+++ repology.py
@@ -18,6 +18,7 @@
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import os.path
 import sys
 import subprocess
 import csv
@@ -148,6 +149,68 @@ class DebianSourcesProcessor(RepositoryProcessor):
 
         return result
 
+class GentooGitProcessor(RepositoryProcessor):
+    src = None
+    path = None
+
+    def __init__(self, path, src):
+        self.path = path
+        self.src = src
+
+    def IsUpToDate(self):
+        return os.path.isdir(self.path)
+
+    def Download(self):
+        if os.path.isdir(self.path):
+            subprocess.check_call("cd %s && git pull -q" % (self.path), shell = True)
+        subprocess.check_call("git clone -q --depth=1 %s %s" % (self.src, self.path), shell = True)
+
+    @staticmethod
+    def SanitizeVersion(version):
+        pos = version.find('-')
+        if pos != -1:
+            version = version[0:pos]
+
+        pos = version.find('_')
+        if pos != -1:
+            version = version[0:pos]
+
+        return version
+
+    def Parse(self):
+        result = []
+
+        for category in os.listdir(self.path):
+            category_path = os.path.join(self.path, category)
+            if not os.path.isdir(category_path):
+                continue
+            for package in os.listdir(category_path):
+                package_path = os.path.join(category_path, package)
+                if not os.path.isdir(package_path):
+                    continue
+
+                maxversion = None
+                bestebuild = None
+                for ebuild in os.listdir(package_path):
+                    if not ebuild.endswith(".ebuild"):
+                        continue
+                    ebuild_path = os.path.join(package_path, ebuild)
+
+                    version = ebuild[len(package)+1:-7]
+
+                    if maxversion is None or (version != "9999" and (maxversion == "9999" or parse_version(version) > parse_version(maxversion))):
+                        maxversion = version
+                        bestebuild = ebuild
+
+                if not maxversion is None:
+                    result.append({
+                        'name': package,
+                        'version': self.SanitizeVersion(maxversion),
+                        'category': category,
+                    })
+
+        return result
+
 REPOSITORIES = [
     { 'name': "FreeBSD Ports", 'processor': FreeBSDIndexProcessor("freebsd.list",
         "http://www.FreeBSD.org/ports/INDEX-11.bz2"
@@ -179,6 +242,9 @@ REPOSITORIES = [
     #    "http://ftp.ubuntu.com/ubuntu/dists/yakkety/restricted/source/Sources.gz",
     #    "http://ftp.ubuntu.com/ubuntu/dists/yakkety/universe/source/Sources.gz"
     #) },
+    { 'name': 'Gentoo', 'processor': GentooGitProcessor("gentoo.git",
+        "https://github.com/gentoo/gentoo.git"
+    ) },
 ]
 
 def MixRepositories(repositories):
