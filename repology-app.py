8736702959e21f4f5d62352428988919a328be8b
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-12-09 20:07:19 +0300

    Add link to repology.org

--- repology-app.py
+++ repology-app.py
@@ -71,7 +71,11 @@ def badge_all(name):
             'versionclass': summary['versionclass']
         })
 
-    return flask.render_template("badge-big.svg", repositories=sorted(repostates, key=lambda repo: repo['name']))
+    return flask.render_template(
+            "badge-big.svg",
+            repositories=sorted(repostates, key=lambda repo: repo['name']),
+            name=name
+    )
 
 @app.route("/news")
 def news():
