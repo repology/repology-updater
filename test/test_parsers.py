849fd0f375914c7f13a5cdcce4ec86d428509b2f
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-12-20 20:11:20 +0300

    Improve package tests

--- test/test_parsers.py
+++ test/test_parsers.py
@@ -26,50 +26,84 @@ from repology.package import Package
 class TestParsers(unittest.TestCase):
     def setUp(self):
         repoman = RepositoryManager("testdata")
-        self.packages = repoman.ParseMulti(reponames=['freebsd', 'gentoo', 'arch'])
+        self.packages = repoman.ParseMulti(reponames=['have_testdata'])
 
-    def FindPackage(self, name):
+    def check_package(self, name, reference):
+        reference_with_default = {
+            # repo must be filled
+            # family must be filled
+
+            # name must be filled
+            'effname': None,
+
+            # version must be filled
+            'origversion': None,
+            'effversion': None,
+            'versionclass': None,
+
+            'maintainers': [],
+            'category': None,
+            'comment': None,
+            'homepage': None,
+            'licenses': [],
+            'downloads': [],
+
+            'ignore': False,
+            'shadow': False,
+            'ignoreversion': False,
+        }
+
+        reference_with_default.update(reference)
+
         for package in self.packages:
             if package.name == name:
-                return package
+                self.assertEqual(package.__dict__, reference_with_default)
+                return
 
-        return Package()
+        self.assertFalse("package not found")
 
     def test_freebsd(self):
-        p = self.FindPackage("vorbis-tools")
-        self.assertEqual(p.repo, "freebsd")
-        self.assertEqual(p.family, "freebsd")
-        self.assertEqual(p.name, "vorbis-tools")
-        self.assertEqual(p.version, "1.4.0")
-        self.assertEqual(p.origversion, "1.4.0_10,3")
-        self.assertEqual(p.category, "audio")
-        self.assertEqual(p.comment, "Play, encode, and manage Ogg Vorbis files")
-        self.assertEqual(p.maintainers, ['naddy@freebsd.org'])
-        self.assertEqual(p.homepage, 'http://www.vorbis.com/')
+        self.check_package("vorbis-tools",
+            {
+                'repo': "freebsd",
+                'family': "freebsd",
+                'name': "vorbis-tools",
+                'version': "1.4.0",
+                'origversion': "1.4.0_10,3",
+                'category': "audio",
+                'comment': "Play, encode, and manage Ogg Vorbis files",
+                'maintainers': ['naddy@freebsd.org'],
+                'homepage': 'http://www.vorbis.com/',
+            }
+        )
 
     def test_gentoo(self):
-        p = self.FindPackage("chromium-bsu")
-        self.assertEqual(p.repo, "gentoo")
-        self.assertEqual(p.family, "gentoo")
-        self.assertEqual(p.name, "chromium-bsu")
-        self.assertEqual(p.version, "0.9.15.1")
-        self.assertEqual(p.origversion, None)
-        self.assertEqual(p.category, "games-action")
-        #self.assertEqual(p.comment, "Chromium B.S.U. - an arcade game")
-        self.assertEqual(p.maintainers, ['games@gentoo.org'])
-        #self.assertEqual(p.homepage, 'http://chromium-bsu.sourceforge.net/')
+        self.check_package("chromium-bsu",
+            {
+                "repo": "gentoo",
+                "family": "gentoo",
+                "name": "chromium-bsu",
+                "version": "0.9.15.1",
+                "origversion": None,
+                "category": "games-action",
+                "maintainers": ['games@gentoo.org'],
+            }
+        )
 
     def test_arch(self):
-        p = self.FindPackage("zlib")
-        self.assertEqual(p.repo, "arch")
-        self.assertEqual(p.family, "arch")
-        self.assertEqual(p.name, "zlib")
-        self.assertEqual(p.version, "1.2.8")
-        self.assertEqual(p.origversion, "1:1.2.8-7")
-        self.assertEqual(p.comment, "Compression library implementing the deflate compression method found in gzip and PKZIP")
-        self.assertEqual(p.homepage, "http://www.zlib.net/")
-        self.assertEqual(p.licenses, ['custom'])
-        self.assertEqual(p.maintainers, ['pierre@archlinux.de'])
+        self.check_package("zlib",
+            {
+                "repo": "arch",
+                "family": "arch",
+                "name": "zlib",
+                "version": "1.2.8",
+                "origversion": "1:1.2.8-7",
+                "comment": "Compression library implementing the deflate compression method found in gzip and PKZIP",
+                "homepage": "http://www.zlib.net/",
+                "licenses": ['custom'],
+                "maintainers": ['pierre@archlinux.de'],
+            }
+        )
 
 if __name__ == '__main__':
     unittest.main()
