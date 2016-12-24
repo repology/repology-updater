17691fdd171478d597be28e9e8423b4a8b03ecd8
Dmitry Marakasov <amdmi3@amdmi3.ru>
2016-12-24 15:09:00 +0300

    Add support for GNU Guix

---
+++ repology/fetcher/guix.py
@@ -0,0 +1,54 @@
+# Copyright (C) 2016 Dmitry Marakasov <amdmi3@amdmi3.ru>
+#
+# This file is part of repology
+#
+# repology is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# repology is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with repology.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import shutil
+
+from repology.logger import NoopLogger
+from repology.www import Get
+
+
+class GuixFetcher():
+    def __init__(self, url):
+        self.url = url
+
+    def DoFetch(self, statepath, update, logger):
+        pages = [ chr(x) for x in range(ord('a'), ord('z') + 1) ] #  a..z
+        pages.append('0-9')
+
+        for page in pages:
+            logger.Log("fetching page " + page)
+            pageurl = self.url + "/" + page + ".html"
+            with open(os.path.join(statepath, page + ".html"), "w", encoding="utf-8") as pagefile:
+                pagefile.write(Get(pageurl).text)
+
+    def Fetch(self, statepath, update=True, logger=NoopLogger()):
+        if os.path.isdir(statepath) and not update:
+            logger.Log("no update requested, skipping")
+            return
+
+        if os.path.exists(statepath):
+            shutil.rmtree(statepath)
+
+        os.mkdir(statepath)
+
+        try:
+            self.DoFetch(statepath, update, logger)
+        except:
+            if os.path.exists(statepath):
+                shutil.rmtree(statepath)
+            raise
---
+++ repology/parser/guix.py
@@ -0,0 +1,65 @@
+# Copyright (C) 2016 Dmitry Marakasov <amdmi3@amdmi3.ru>
+#
+# This file is part of repology
+#
+# repology is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# repology is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with repology.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import xml.etree.ElementTree
+
+from repology.package import Package
+
+
+class GuixParser():
+    def __init__(self):
+        pass
+
+    def Parse(self, path):
+        result = []
+
+        for filename in os.listdir(path):
+            if not filename.endswith(".html"):
+                continue
+
+            root = xml.etree.ElementTree.parse(os.path.join(path, filename))
+
+            table = root.find(".//table[@id='packages']")
+
+            first = True
+            for row in table.findall("tr"):
+                if first:
+                    first = False
+                    continue
+
+                pkg = Package()
+
+                # name + version
+                cell = row.find("./td[2]/a")
+                pkg.name, pkg.version = cell.text.split(' ', 2)
+
+                # summary
+                cell = row.find("./td[3]/span")
+                pkg.comment = cell.text
+
+                # licenses
+                for cell in row.findall("./td[3]/div[1]/div[2]/a[@title='Link to the full license']"):
+                    pkg.licenses.append(cell.text)
+
+                # www
+                cell = row.find("./td[3]/div[1]/a[@title=\"Link to the package's website\"]")
+                pkg.homepage = cell.attrib['href']
+
+                result.append(pkg)
+
+        return result
