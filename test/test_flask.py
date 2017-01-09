32dfae5ec74375caae5b36f4406f884121187627
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-01-09 15:00:12 +0300

    Fix test, there's no more badges page

--- test/test_flask.py
+++ test/test_flask.py
@@ -56,7 +56,6 @@ class TestFlask(unittest.TestCase):
         self.checkurl('/news', has=['support added']);
         self.checkurl('/about', has=['maintainers']);
         self.checkurl('/api', has=['/api/v1/metapackages/all/firefox']);
-        self.checkurl('/badges', has=['http://repology.org/badge/vertical-allrepos/METAPACKAGE', 'http://repology.org/badge/tiny-packages/METAPACKAGE']);
 
     def test_statistics(self):
         self.checkurl('/statistics', has=['FreeBSD']);
