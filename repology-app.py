020a0051712cec40dc0fd500666c5694df5c31b8
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-02-16 17:55:08 +0300

    Simplify profiling

--- repology-app.py
+++ repology-app.py
@@ -20,6 +20,7 @@
 import json
 
 import flask
+from werkzeug.contrib.profiler import ProfilerMiddleware
 
 from repology.database import Database
 from repology.metapackageproc import *
@@ -684,4 +685,8 @@ def api_v1_metapackages_outdated_by_maintainer(maintainer, bound=None):
 
 
 if __name__ == '__main__':
-    app.run()
+    if app.config['PROFILE']:
+        app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
+        app.run(debug = True)
+    else:
+        app.run()
