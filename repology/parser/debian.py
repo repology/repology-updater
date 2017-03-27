90d9513124832f972469eb74be5f9d157a710079
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-03-27 20:19:30 +0300

    Make Debian parser more flexible

--- repology/parser/debian.py
+++ repology/parser/debian.py
@@ -16,6 +16,7 @@
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
 import re
+import sys
 
 from repology.package import Package
 from repology.util import GetMaintainers
@@ -62,23 +63,65 @@ class DebianSourcesParser():
         result = []
 
         with open(path, encoding='utf-8') as file:
-            pkg = Package()
+            current_data = {}
+            last_key = None
+
             for line in file:
-                line = line.strip()
+                line = line.rstrip('\n')
+
+                # empty line, dump package
                 if line == '':
-                    result.append(pkg)
                     pkg = Package()
-                elif line.startswith('Package: '):
-                    pkg.name = line[9:]
-                elif line.startswith('Version: '):
-                    pkg.version, pkg.origversion = SanitizeVersion(line[9:])
-                elif line.startswith('Maintainer: '):
-                    pkg.maintainers += GetMaintainers(line[12:])
-                elif line.startswith('Uploaders: '):
-                    pkg.maintainers += GetMaintainers(line[11:])
-                elif line.startswith('Section: '):
-                    pkg.category = line[9:]
-                elif line.startswith('Homepage: '):
-                    pkg.homepage = line[10:]
+
+                    def GetField(key, type_=str, default=None):
+                        if key in current_data:
+                            if type_ is None or isinstance(current_data[key], type_):
+                                return current_data[key]
+                            else:
+                                print('WARNING: unable to parse field {}'.format(key), file=sys.stderr)
+                                return default
+                        else:
+                            return default
+
+                    pkg.name = GetField('Package')
+                    pkg.version, pkg.origversion = SanitizeVersion(GetField('Version'))
+                    pkg.maintainers += GetMaintainers(GetField('Maintainer', default=''))
+                    pkg.maintainers += GetMaintainers(GetField('Uploaders', default=''))
+                    pkg.category = GetField('Section')
+                    pkg.homepage = GetField('Homepage')
+
+                    # This is long description
+                    #pkg.comment = GetField('Description', type_=None)
+                    #if isinstance(pkg.comment, list):
+                    #    pkg.comment = ' '.join(pkg.comment)
+
+                    if pkg.name and pkg.version:
+                        result.append(pkg)
+                    else:
+                        print('WARNING: unable to parse package {}'.format(str(current_data)), file=sys.stderr)
+
+                    current_data = {}
+                    last_key = None
+                    continue
+
+                # key - value pair
+                match = re.match('([A-Za-z0-9-]+):(.*?)$', line)
+                if match:
+                    key = match.group(1)
+                    value = match.group(2).strip()
+                    current_data[key] = value
+                    last_key = key
+                    continue
+
+                # continuation of previous key
+                match = re.match(' (.*)$', line)
+                if match:
+                    value = match.group(1).strip()
+                    if not isinstance(current_data[last_key], list):
+                        current_data[last_key] = [current_data[last_key]]
+                    current_data[last_key].append(value)
+                    continue
+
+                print('WARNING: unable to parse line: {}'.format(line), file=sys.stderr)
 
         return result
