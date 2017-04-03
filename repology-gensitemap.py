0314357332cb51a82769257dc27da48173e2cb80
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-04-03 14:50:34 +0300

    Improve sitemap generator

--- repology-gensitemap.py
+++ repology-gensitemap.py
@@ -33,63 +33,64 @@ def Main():
     parser.add_argument('-w', '--www-home', default=repology.config.REPOLOGY_HOME, help='repology www home')
     parser.add_argument('-m', '--max-urls', default=50000, help='max number of urls to generate')
 
+    parser.add_argument('--main', action='store_true', help='generate maintainers sitemap')
+    parser.add_argument('--metapackages', action='store_true', help='generate maintainers sitemap')
+
     options = parser.parse_args()
 
     database = Database(options.dsn, readonly=True)
 
-    print('Adding static links', file=sys.stderr)
+    urls = []
+    if options.main:
+        urls = ['/', '/news', '/statistics', '/about', '/api/v1', '/repositories/']
 
-    # static pages
-    urls = ['/', '/news', '/statistics', '/about', '/api/v1', '/repositories/']
+        urls.extend(map(lambda row: '/maintainer/' + row[0], database.Query('SELECT maintainer FROM maintainers')))
+        urls.extend(map(lambda row: '/repository/' + row[0], database.Query('SELECT name FROM repositories')))
+    elif options.metapackages:
+        LINKS_PER_METAPACKAGE = 3
 
-    # maintainer lists
-    urls.append('/maintainers/')
-    for letter in 'abcdefghijklmnopqrstuvwxyz':
-        urls.append('/maintainers/' + letter + '/')
+        print('Guessing threshold for important metapackages', file=sys.stderr)
 
-    # metapackages
-    LINKS_PER_METAPACKAGE = 3
+        num_repos = 1
+        while True:
+            num_metapackages = database.Query(
+                'SELECT count(DISTINCT effname) FROM metapackage_repocounts WHERE num_families >= %s',
+                num_repos
+            )[0][0]
 
-    print('Guessing threshold for important metapackages', file=sys.stderr)
+            num_urls_total = len(urls) + num_metapackages * LINKS_PER_METAPACKAGE
 
-    num_repos = 1
-    while True:
-        num_metapackages = database.Query(
-            'SELECT count(DISTINCT effname) FROM metapackage_repocounts WHERE num_families >= %s',
-            num_repos
-        )[0][0]
+            print('Threshold = {}, {} metapackages, {} total urls'.format(num_repos, num_metapackages, num_urls_total), file=sys.stderr)
 
-        num_urls_total = len(urls) + num_metapackages * LINKS_PER_METAPACKAGE
+            if num_urls_total <= options.max_urls:
+                print('  Looks good', file=sys.stderr)
+                break
 
-        print('Threshold = {}, {} metapackages, {} total urls'.format(num_repos, num_metapackages, num_urls_total), file=sys.stderr)
+            if num_repos > 20:
+                print('  Giving up, will truncate metapackage list', file=sys.stderr)
+                break
 
-        if num_urls_total <= options.max_urls:
-            print('  Looks good', file=sys.stderr)
-            break
+            num_repos += 1
 
-        if num_repos > 20:
-            print('  Giving up, will truncate metapackage list', file=sys.stderr)
-            break
+        # get most important packages
+        for row in database.Query(
+                'SELECT DISTINCT effname FROM metapackage_repocounts WHERE num_families >= %s LIMIT %s',
+                num_repos,
+                (options.max_urls - len(urls)) // LINKS_PER_METAPACKAGE):
+            urls.append('/metapackage/' + row[0] + '/versions')
+            urls.append('/metapackage/' + row[0] + '/packages')
+            urls.append('/metapackage/' + row[0] + '/information')
 
-        num_repos += 1
-
-    # get most important packages
-    for row in database.Query(
-            'SELECT DISTINCT effname FROM metapackage_repocounts WHERE num_families >= %s LIMIT %s',
-            num_repos,
-            (options.max_urls - len(urls)) // LINKS_PER_METAPACKAGE):
-        urls.append('/metapackage/' + row[0] + '/versions')
-        urls.append('/metapackage/' + row[0] + '/packages')
-        urls.append('/metapackage/' + row[0] + '/information')
-
-    # fill the remaining space with less important packages
-    for row in database.Query(
-            'SELECT DISTINCT effname FROM metapackage_repocounts WHERE num_families = %s LIMIT %s',
-            num_repos - 1,
-            (options.max_urls - len(urls)) // LINKS_PER_METAPACKAGE):
-        urls.append('/metapackage/' + row[0] + '/versions')
-        urls.append('/metapackage/' + row[0] + '/packages')
-        urls.append('/metapackage/' + row[0] + '/information')
+        # fill the remaining space with less important packages
+        for row in database.Query(
+                'SELECT DISTINCT effname FROM metapackage_repocounts WHERE num_families = %s LIMIT %s',
+                num_repos - 1,
+                (options.max_urls - len(urls)) // LINKS_PER_METAPACKAGE):
+            urls.append('/metapackage/' + row[0] + '/versions')
+            urls.append('/metapackage/' + row[0] + '/packages')
+            urls.append('/metapackage/' + row[0] + '/information')
+    else:
+        print('Please specify output mode', file=sys.stderr)
 
     shuffle(urls)
 
