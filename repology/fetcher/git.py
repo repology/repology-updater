a3d479a30182dc049d5a33070eb304e817255e79
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-04-07 17:44:52 +0300

    Add timeout for git fetch

--- repology/fetcher/git.py
+++ repology/fetcher/git.py
@@ -30,7 +30,7 @@ class GitFetcher():
         if not os.path.isdir(statepath):
             RunSubprocess(['git', 'clone', '--progress', '--depth=1', self.url, statepath], logger=logger)
         elif update:
-            RunSubprocess(['git', 'fetch', '--progress', '--depth=1'], cwd=statepath, logger=logger)
+            RunSubprocess(['timeout', '10m', 'git', 'fetch', '--progress', '--depth=1'], cwd=statepath, logger=logger)
             RunSubprocess(['git', 'reset', '--hard', 'origin/' + self.branch], cwd=statepath, logger=logger)
             RunSubprocess(['git', 'reflog', 'expire', '--expire=0', '--all'], cwd=statepath, logger=logger)
             RunSubprocess(['git', 'prune'], cwd=statepath, logger=logger)
