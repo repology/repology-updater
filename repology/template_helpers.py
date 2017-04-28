cc07c2a4698bcc6f2327be5ef5bab2d3ae4a0f7c
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-04-28 21:49:07 +0300

    Remove unused imports

--- repology/template_helpers.py
+++ repology/template_helpers.py
@@ -17,11 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-import re
-
 import flask
 
-from repology.package import *
 from repology.packageformatter import PackageFormatter
 
 
