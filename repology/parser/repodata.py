a5b9c529c90aed54a72be52582080a1c558e4ceb
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-05-11 20:22:17 +0300

    Fix plus suffix stripping for repodata parser
    This affects opensuse mostly, for which versions like
    0.3.1+git20160217.7897d3f are common. Because of code bug,
    this was converted to git20160217.7897d3f instead of 0.3.1

--- repology/parser/repodata.py
+++ repology/parser/repodata.py
@@ -25,7 +25,7 @@ def SanitizeVersion(version):
 
     pos = version.find('+')
     if pos != -1:
-        version = version[pos + 1:]
+        version = version[:pos]
 
     if version != origversion:
         return version, origversion
