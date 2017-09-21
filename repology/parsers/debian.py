d172a4f920260608d4be84d883ecfb1d3e3b0834
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-09-21 14:37:44 +0300

    Improve Debian/Ubuntu version normalization
    Preserve SOME of their wild variety of suffixes such as ~betaN, to
    make a lot of prerelease versions no longer look as releases
    
    Fixes #216

--- repology/parsers/debian.py
+++ repology/parsers/debian.py
@@ -25,29 +25,30 @@ from repology.util import GetMaintainers
 def SanitizeVersion(version):
     origversion = version
 
+    # epoch
     pos = version.find(':')
     if pos != -1:
         version = version[pos + 1:]
 
-    pos = version.find('-')
+    # revision
+    pos = version.rfind('-')
     if pos != -1:
         version = version[0:pos]
 
-    pos = version.find('+')
-    if pos != -1:
-        version = version[0:pos]
+    # garbage debian/ubuntu addendums
+    version = re.sub('[.~+-]?(dfsg|ubuntu).*', '', version)
 
-    pos = version.find('~')
-    if pos != -1:
-        version = version[0:pos]
+    # remove suffixes
+    version, *suffixes = re.split('[~+-]', version)
 
-    match = re.match('(.*[0-9])[^0-9]*dfsg\\.?[0-9]*$', version)
-    if match is not None:
-        version = match.group(1)
+    # append useful suffixes
+    good_suffixes = []
+    for suffix in suffixes:
+        match = re.match('((?:a|b|r|alpha|beta|rc|pre|patch|git|svn|cvs|hg|bzr|nmu|darcs)[-]?[0-9]+(?:\\.[0-9]+)*)', suffix)
+        if match:
+            good_suffixes.append(match.group(1))
 
-    match = re.match('(.*[0-9])ubuntu[0-9.]+$', version)
-    if match is not None:
-        version = match.group(1)
+    version += '.'.join(good_suffixes)
 
     if version != origversion:
         return version, origversion
