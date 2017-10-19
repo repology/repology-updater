5c5c016cb37d4ae8f7865449370568debb26d68f
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-10-19 15:56:32 +0300

    Add font measurer class
    This will be used to calculate badge width on the fly

---
+++ test/test_fontmeasurer.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2017 Dmitry Marakasov <amdmi3@amdmi3.ru>
+#
+# This file is part of repology
+#
+# repology is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# repology is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with repology.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import unittest
+
+from repologyapp.fontmeasurer import FontMeasurer
+
+from repology.config import config
+
+
+@unittest.skipIf('BADGE_FONT' not in config, 'font measurer test requires BADGE_FONT configuration directive defined')
+class FontMeasurerTest(unittest.TestCase):
+    def test_fontmeasurer(self):
+        fontmeas = FontMeasurer(config['BADGE_FONT'], 11)
+
+        self.assertEqual(fontmeas.GetDimensions('A'), (7, 11))
+        self.assertEqual(fontmeas.GetDimensions('A'), (7, 11))  # cached
+
+
+if __name__ == '__main__':
+    unittest.main()
