89efb761239bf1a1678b9633cd0d43e6c2e2ad88
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2017-11-28 22:23:19 +0100

    proper solution using repodata

--- repology/parsers/voidlinux.py
+++ repology/parsers/voidlinux.py
@@ -16,48 +16,29 @@
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-import re
-import sys
-import collections
-import subprocess
+import plistlib
 
 from repology.package import Package
-from repology.version import VersionCompare
 
-SEP = ":\t"
+def parse_maintainer(maintainerstr):
+    if not maintainerstr: return []
+    return [maintainerstr.split()[-1].lstrip('<').rstrip('>')]
 
-class VoidLinuxGitParser():
+class VoidLinuxParser():
+
+    def __init__(self):
+        pass
 
     def Parse(self, path):
-        packages = []
+        index_path = os.path.join(path, 'index.plist')
+        plist_index = plistlib.load(open(index_path, 'rb'),
+                                    fmt=plistlib.FMT_XML)
 
-        source_path = os.path.join(path, 'srcpkgs')
-        xbps_src_path = os.path.join(path, 'xbps-src')
-        for package_name in os.listdir(source_path):
-            result = subprocess.run(
-                [xbps_src_path, "show", package_name],
-                stdout=subprocess.PIPE,
-                encoding="UTF-8"
-            )
-        
-            if result.returncode != 0:
-                print("Failed to read template for {}".format(package_name), file=sys.stderr)
-                continue
-        
-            variables = collections.defaultdict(list)
-            for line in result.stdout.splitlines():
-                variable, *rest = line.split(SEP)
-                if rest: variables[variable].append(SEP.join(rest))
-                                    
-            pkg = Package()
-            pkg.name = variables["pkgname"][0]
-            pkg.version = variables["version"][0] + "-" + variables["revision"][0]
-            pkg.licenses = [l.strip() for l in variables["License(s)"][0].split(',')]
-            pkg.comment = variables["short_desc"][0]
-            pkg.homepage = variables["Upstream URL"][0]
-            pkg.downloads = variables["distfiles"]
-
-            packages.append(pkg)
-
-        return packages
+        return [ Package(name=pkgname,
+                         version=props['pkgver'].split('-')[-1].replace('_', '-'),
+                         maintainers=parse_maintainer(props.get('maintainer')),
+                         comment=props['short_desc'],
+                         homepage=props['homepage'],
+                         licenses=[l.strip() for l in props['license'].split(',')])
+                 for pkgname, props in plist_index.items() ]
 
