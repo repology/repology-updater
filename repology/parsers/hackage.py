b8ef396b4a412550ed9609d1c86018b9d061ae49
Dmitry Marakasov <amdmi3@amdmi3.ru>
2017-12-05 15:49:35 +0300

    Parse more info from hackage

--- repology/parsers/hackage.py
+++ repology/parsers/hackage.py
@@ -16,6 +16,8 @@
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import re
+import sys
 
 from repology.package import Package
 from repology.version import VersionCompare
@@ -25,12 +27,56 @@ class HackageParser():
     def __init__(self):
         pass
 
+    def ParseCabal(self, path):
+        cabaldata = {}
+        offset = None
+        key = None
+
+        with open(path) as cabalfile:
+            for line in cabalfile:
+                line = line.rstrip()
+
+                # offset is needed to be calculated first, from first non-whitespace line
+                if offset is None:
+                    match = re.match('([ ]*)[^ ]', line)
+                    if match:
+                        offset = len(match.group(1))
+                    else:
+                        continue
+
+                line = line[offset:]
+
+                # ignore comments
+                if line.startswith('--'):
+                    continue
+
+                # process multiline keys
+                if key:
+                    if line.startswith(' '):
+                        cabaldata[key] = cabaldata[key] + ' ' + line.strip() if key in cabaldata else line.strip()
+                        continue
+                    else:
+                        key = None
+
+                # process singleline key or start of a multiline key
+                match = re.fullmatch('([a-zA-Z-]+)[ \t]*:[ \t]*(.*?)', line)
+                if not match:
+                    continue
+
+                if match.group(2):
+                    cabaldata[match.group(1).lower()] = match.group(2)
+                else:
+                    key = match.group(1).lower()
+
+        return cabaldata
+
     def Parse(self, path):
         packages = []
 
         for moduledir in os.listdir(path):
             modulepath = os.path.join(path, moduledir)
 
+            cabalpath = None
             maxversion = None
 
             for versiondir in os.listdir(modulepath):
@@ -39,14 +85,30 @@ class HackageParser():
 
                 if maxversion is None or VersionCompare(versiondir, maxversion) > 0:
                     maxversion = versiondir
+                    cabalpath = os.path.join(path, moduledir, maxversion, moduledir + '.cabal')
 
             pkg = Package()
-
-            # XXX: parse .cabal file
 
             pkg.name = moduledir
             pkg.version = maxversion
             pkg.homepage = 'http://hackage.haskell.org/package/' + moduledir
+
+            cabaldata = self.ParseCabal(cabalpath)
+
+            if cabaldata['name'] == pkg.name and VersionCompare(cabaldata['version'], pkg.version) == 0:
+                if 'synopsis' in cabaldata and cabaldata['synopsis']:
+                    pkg.comment = cabaldata['synopsis'].strip()
+                # XXX: leave for later, need extra postprocessing, too much obfuscation
+                #if 'maintainer' in cabaldata:
+                #    pkg.maintainers = GetMaintainers(cabaldata['maintainer'])
+                if 'license' in cabaldata:
+                    pkg.licenses = [cabaldata['license']]
+                if 'homepage' in cabaldata and (cabaldata['homepage'].startswith('http://') or cabaldata['homepage'].startswith('https://')):
+                    pkg.homepage = cabaldata['homepage']
+                if 'category' in cabaldata:
+                    pkg.category = cabaldata['category']
+            else:
+                print('WARNING: cabal data sanity check failed for {}, ignoring cabal data'.format(cabalpath), file=sys.stderr)
 
             packages.append(pkg)
 
