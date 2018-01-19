2e19c9539a3c5f129646972d24b02d137ae7ad16
Dmitry Marakasov <amdmi3@amdmi3.ru>
2018-01-19 21:40:03 +0300

    Fix repository statistics retrieval

--- repology/database.py
+++ repology/database.py
@@ -1395,7 +1395,7 @@ class Database:
             SELECT
                 ts AS timestamp,
                 now() - ts AS timedelta,
-                snapshot{}
+                snapshot{} AS snapshot
             FROM repositories_history
             WHERE ts >= now() - INTERVAL %s
             ORDER BY ts
