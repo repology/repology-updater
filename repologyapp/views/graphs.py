6f35b0ff356df541768c635601a990e4e1f5e7ca
Dmitry Marakasov <amdmi3@amdmi3.ru>
2018-02-20 01:33:15 +0300

    Add missing import

--- repologyapp/views/graphs.py
+++ repologyapp/views/graphs.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
+import datetime
 import math
 
 import flask
