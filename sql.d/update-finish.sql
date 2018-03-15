c1d803ee9a5d8a9c24d523ed1964015f05536ef2
Dmitry Marakasov <amdmi3@amdmi3.ru>
2018-03-15 16:22:38 +0300

    Fix SQL

--- sql.d/update-finish.sql
+++ sql.d/update-finish.sql
@@ -129,7 +129,7 @@ DO UPDATE SET
 				metapackages_state.devel_versions != EXCLUDED.devel_versions OR
 				metapackages_state.unique_versions != EXCLUDED.unique_versions
 			THEN now()
-			ELSE last_actual_versions_update
+			ELSE metapackages_state.last_actual_versions_update
 		END,
 	actual_repos = EXCLUDED.actual_repos,
 	all_repos = EXCLUDED.all_repos
