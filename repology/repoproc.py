8c3f2cd1b4f8e30ceccb984c55a5f3978055666f
Dmitry Marakasov <amdmi3@amdmi3.ru>
2018-08-03 19:52:56 +0300

    Switch to new logger interface in RepositoryProcessor

--- repology/repoproc.py
+++ repology/repoproc.py
@@ -23,7 +23,7 @@ import time
 import traceback
 
 from repology.fetchers import Fetcher
-from repology.logger import NoopLogger
+from repology.logger import Logger, NoopLogger
 from repology.moduleutils import ClassFactory
 from repology.package import PackageFlags, PackageSanityCheckFailure, PackageSanityCheckProblem
 from repology.packageproc import PackagesetDeduplicate
@@ -67,19 +67,19 @@ class RepositoryProcessor:
 
     def __CheckRepositoryOutdatedness(self, repository, logger):
         if 'valid_till' in repository and datetime.date.today() >= repository['valid_till']:
-            logger.Log('WARNING: Repository {} has reached EoL, please update configs'.format(repository['name']))
+            logger.log('repository {} has reached EoL, please update configs'.format(repository['name']), severity=Logger.WARNING)
 
     # Private methods which provide single actions on sources
     def __FetchSource(self, update, repository, source, logger):
         if 'fetcher' not in source:
-            logger.Log('fetching source {} not supported'.format(source['name']))
+            logger.log('fetching source {} not supported'.format(source['name']))
             return
 
         fetcher = self.fetcher_factory.SpawnWithKnownArgs(source['fetcher'], source)
 
         ntry = 1
         while ntry <= self.fetch_retries:
-            logger.Log('fetching source {} try {} started'.format(source['name'], ntry))
+            logger.log('fetching source {} try {} started'.format(source['name'], ntry))
 
             try:
                 fetcher.fetch(
@@ -95,19 +95,19 @@ class RepositoryProcessor:
                 if ntry >= self.fetch_retries:
                     raise
 
-                logger.Log('fetching source {} try {} failed:'.format(source['name'], ntry))
+                logger.log('fetching source {} try {} failed:'.format(source['name'], ntry))
                 for item in traceback.format_exception(*sys.exc_info()):
                     for line in item.split('\n'):
                         if line:
-                            logger.GetIndented().Log(line)
+                            logger.GetIndented().log(line, severity=Logger.ERROR)
 
-                logger.Log('waiting {} seconds before retry'.format(self.fetch_retry_delay))
+                logger.log('waiting {} seconds before retry'.format(self.fetch_retry_delay), severity=Logger.WARNING)
                 if self.fetch_retry_delay:
                     time.sleep(self.fetch_retry_delay)
 
             ntry += 1
 
-        logger.Log('fetching source {} complete with {} tries'.format(source['name'], ntry))
+        logger.log('fetching source {} complete with {} tries'.format(source['name'], ntry))
 
     def _iter_parse_source(self, repository, source, logger):
         def postprocess_parsed_packages(packages_iter):
@@ -132,7 +132,7 @@ class RepositoryProcessor:
                     # one hand, but shouldn't stop whole repo from updating on another. In
                     # future, it should be logged as some kind of very serious repository
                     # update error
-                    logger.Log('ERROR: package with empty version: {}'.format(package.name))
+                    logger.log('package with empty version: {}'.format(package.name), severity=Logger.ERROR)
                     continue
 
                 yield package
@@ -147,7 +147,7 @@ class RepositoryProcessor:
 
     # Private methods which provide single actions on repos
     def __Fetch(self, update, repository, logger):
-        logger.Log('fetching started')
+        logger.log('fetching started')
 
         if not os.path.isdir(self.statedir):
             os.mkdir(self.statedir)
@@ -157,35 +157,35 @@ class RepositoryProcessor:
                 os.mkdir(self.__GetRepoPath(repository))
             self.__FetchSource(update, repository, source, logger.GetIndented())
 
-        logger.Log('fetching complete')
+        logger.log('fetching complete')
 
     def _parse(self, repository, logger):
-        logger.Log('parsing started')
+        logger.log('parsing started')
 
         packages = []
 
         for source in repository['sources']:
             usage = ResourceUsageMonitor()
 
-            logger.Log('parsing source {} started'.format(source['name']))
+            logger.log('parsing source {} started'.format(source['name']))
 
             packages.extend(self._iter_parse_source(repository, source, logger.GetIndented()))
 
-            logger.Log('parsing source {} complete, resource usage: {}'.format(source['name'], usage.get_usage_str()))
+            logger.log('parsing source {} complete, resource usage: {}'.format(source['name'], usage.get_usage_str()))
 
-        logger.Log('parsing complete, {} packages, deduplicating'.format(len(packages)))
+        logger.log('parsing complete, {} packages, deduplicating'.format(len(packages)))
 
         packages = PackagesetDeduplicate(packages)
 
         if self.safety_checks and len(packages) < repository['minpackages']:
             raise TooLittlePackages(len(packages), repository['minpackages'])
 
-        logger.Log('parsing complete, {} packages'.format(len(packages)))
+        logger.log('parsing complete, {} packages'.format(len(packages)))
 
         return packages
 
     def __Transform(self, packages, transformer, repository, logger):
-        logger.Log('processing started')
+        logger.log('processing started')
         sanitylogger = logger.GetIndented()
         for package in packages:
             package.repo = repository['name']
@@ -208,10 +208,10 @@ class RepositoryProcessor:
             try:
                 package.CheckSanity(transformed=transformer is not None)
             except PackageSanityCheckFailure as err:
-                sanitylogger.Log('sanity error: {}'.format(err))
+                sanitylogger.log('sanity error: {}'.format(err), severity=Logger.ERROR)
                 raise
             except PackageSanityCheckProblem as err:
-                sanitylogger.Log('sanity warning: {}'.format(err))
+                sanitylogger.log('sanity warning: {}'.format(err), severity=Logger.WARNING)
 
             package.Normalize()
 
@@ -220,23 +220,23 @@ class RepositoryProcessor:
         # package listing
         packages = [package for package in packages if not package.HasFlag(PackageFlags.remove)]
 
-        logger.Log('processing complete, {} packages, deduplicating'.format(len(packages)))
+        logger.log('processing complete, {} packages, deduplicating'.format(len(packages)))
 
         packages = PackagesetDeduplicate(packages)
 
         if transformer:
-            logger.Log('processing complete, {} packages, sorting'.format(len(packages)))
+            logger.log('processing complete, {} packages, sorting'.format(len(packages)))
 
             packages = sorted(packages, key=lambda package: package.effname)
 
-        logger.Log('processing complete, {} packages'.format(len(packages)))
+        logger.log('processing complete, {} packages'.format(len(packages)))
 
         return packages
 
     def __Serialize(self, packages, path, repository, logger):
         tmppath = path + '.tmp'
 
-        logger.Log('saving started')
+        logger.log('saving started')
         with open(tmppath, 'wb') as outfile:
             pickler = pickle.Pickler(outfile, protocol=pickle.HIGHEST_PROTOCOL)
             pickler.fast = True  # deprecated, but I don't see any alternatives
@@ -244,18 +244,18 @@ class RepositoryProcessor:
             for package in packages:
                 pickler.dump(package)
         os.replace(tmppath, path)
-        logger.Log('saving complete, {} packages'.format(len(packages)))
+        logger.log('saving complete, {} packages'.format(len(packages)))
 
     def __Deserialize(self, path, repository, logger):
         packages = []
-        logger.Log('loading started')
+        logger.log('loading started')
         with open(path, 'rb') as infile:
             unpickler = pickle.Unpickler(infile)
             numpackages = unpickler.load()
             packages = [unpickler.load() for num in range(0, numpackages)]
             if packages and not packages[0].CheckFormat():
                 raise StateFileFormatCheckProblem(path)
-        logger.Log('loading complete, {} packages'.format(len(packages)))
+        logger.log('loading complete, {} packages'.format(len(packages)))
 
         return packages
 
