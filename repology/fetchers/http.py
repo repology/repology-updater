71aef9d20548197c8f283d28b142883f93f652ac
Dmitry Marakasov <amdmi3@amdmi3.ru>
2018-11-16 20:00:15 +0300

    Style fix

--- repology/fetchers/http.py
+++ repology/fetchers/http.py
@@ -15,9 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-import time
 import functools
-
+import time
 from json import dumps
 
 import requests
