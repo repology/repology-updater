06d5d9c9221ac27ed7b75b68ce06a43619c1bde5
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-01-29 19:45:56 +0300

    Switch ravenports to JsonSlicer

--- repology/parsers/parsers/ravenports.py
+++ repology/parsers/parsers/ravenports.py
@@ -15,30 +15,27 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-import json
+from jsonslicer import JsonSlicer
 
 from repology.parsers import Parser
 
 
 class RavenportsJsonParser(Parser):
     def iter_parse(self, path, factory, transformer):
-        jsondata = None
-        with open(path, 'r', encoding='utf-8') as jsonfile:
-            jsondata = json.load(jsonfile)
+        with open(path, 'rb') as jsonfile:
+            for packagedata in JsonSlicer(jsonfile, ('ravenports', None)):
+                pkg = factory.begin()
 
-        for packagedata in jsondata['ravenports']:
-            pkg = factory.begin()
+                pkg.set_name(packagedata['namebase'])
+                pkg.set_version(packagedata['version'])
+                pkg.add_categories(packagedata['keywords'])
+                pkg.add_homepages(packagedata.get('homepage'))
 
-            pkg.set_name(packagedata['namebase'])
-            pkg.set_version(packagedata['version'])
-            pkg.add_categories(packagedata['keywords'])
-            pkg.add_homepages(packagedata.get('homepage'))
+                pkg.add_downloads(packagedata['distfile'])
+                pkg.set_summary(packagedata['variants'][0]['sdesc'])
+                pkg.add_maintainers(map(lambda contact: contact.get('email'), packagedata.get('contacts', [])))
 
-            pkg.add_downloads(packagedata['distfile'])
-            pkg.set_summary(packagedata['variants'][0]['sdesc'])
-            pkg.add_maintainers(map(lambda contact: contact.get('email'), packagedata.get('contacts', [])))
+                pkg.set_extra_field('bucket', packagedata['bucket'])
+                pkg.set_extra_field('variant', packagedata['variants'][0]['label'])
 
-            pkg.set_extra_field('bucket', packagedata['bucket'])
-            pkg.set_extra_field('variant', packagedata['variants'][0]['label'])
-
-            yield pkg
+                yield pkg
