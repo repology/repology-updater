fbb5e44c9cdbf9dffbc7acde97f509018aedaae6
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-02-07 23:23:45 +0300

    Fix license parsing in Solus

--- repology/parsers/parsers/solus.py
+++ repology/parsers/parsers/solus.py
@@ -32,6 +32,10 @@ def _iter_package_entries(path):
             elem.clear()
 
 
+def _expand_multiline_licenses(text):
+    return (license.lstrip('- ') for license in text.split('\n'))
+
+
 class SolusIndexParser(Parser):
     def iter_parse(self, path, factory, transformer):
         for entry in _iter_package_entries(path):
@@ -43,8 +47,8 @@ class SolusIndexParser(Parser):
 
                 pkg.set_name(namenode.text)
                 pkg.set_summary(entry.find('Summary').text)
-                pkg.add_licenses(map(lambda el: el.text, entry.findall('License')))
-                pkg.add_categories(map(lambda el: el.text, entry.findall('PartOf')))
+                pkg.add_licenses((_expand_multiline_licenses(elt.text) for elt in entry.findall('License')))
+                pkg.add_categories((elt.text for elt in entry.findall('PartOf')))
 
                 for update in entry.find('History').findall('Update'):
                     pkg.set_version(update.find('Version').text)
