0de4a4b744557dffc8a21c8d5eb9e208bd8349ec
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-03-05 15:09:22 +0300

    Update get_repositories_update_diagnostics after removing run-related fields from repositoreis table

--- sql.d/get_repositories_update_diagnostics.sql
+++ sql.d/get_repositories_update_diagnostics.sql
@@ -1,4 +1,4 @@
--- Copyright (C) 2016-2018 Dmitry Marakasov <amdmi3@amdmi3.ru>
+-- Copyright (C) 2016-2019 Dmitry Marakasov <amdmi3@amdmi3.ru>
 --
 -- This file is part of repology
 --
@@ -23,22 +23,33 @@
 SELECT
 	name,
 
-	last_successful_fetch_run_id as fetch_run_id,
-	last_successful_parse_run_id as parse_run_id,
-
+	fetch_run.id as fetch_run_id,
 	fetch_run.finish_ts - fetch_run.start_ts AS fetch_duration,
 	fetch_run.utime AS fetch_utime,
 	fetch_run.stime AS fetch_stime,
 	fetch_run.maxrss AS fetch_maxrss,
 	CASE fetch_run.maxrss_delta WHEN 0 THEN NULL ELSE fetch_run.maxrss END AS fetch_maxrss_reliable,
 
+	parse_run.id as parse_run_id,
 	parse_run.finish_ts - parse_run.start_ts AS parse_duration,
 	parse_run.utime AS parse_utime,
 	parse_run.stime AS parse_stime,
 	parse_run.maxrss AS parse_maxrss,
 	CASE parse_run.maxrss_delta WHEN 0 THEN NULL ELSE parse_run.maxrss END AS parse_maxrss_reliable
 FROM repositories
-LEFT JOIN runs AS fetch_run ON (fetch_run.id = last_successful_fetch_run_id)
-LEFT JOIN runs AS parse_run ON (parse_run.id = last_successful_parse_run_id)
+LEFT JOIN (
+	SELECT
+        *,
+        row_number() over(PARTITION BY repository_id ORDER BY start_ts DESC) AS rn
+    FROM runs
+    WHERE successful AND type='fetch'::run_type
+) fetch_run ON fetch_run.repository_id = repositories.id AND fetch_run.rn = 1
+LEFT JOIN (
+	SELECT
+        *,
+        row_number() over(PARTITION BY repository_id ORDER BY start_ts DESC) AS rn
+    FROM runs
+    WHERE successful AND type='parse'::run_type
+) parse_run ON parse_run.repository_id = repositories.id AND parse_run.rn = 1
 WHERE state != 'legacy'::repository_state
 ORDER BY sortname;
