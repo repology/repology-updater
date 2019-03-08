567c31954d9da4f5aed2f1620a9649b73f257f5b
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-03-09 02:16:27 +0300

    Fix package reference in RosaXmlParser, and switch to PackageMager context manager

--- repology/parsers/parsers/rosa.py
+++ repology/parsers/parsers/rosa.py
@@ -31,24 +31,23 @@ class RosaInfoXmlParser(Parser):
         root = xml.etree.ElementTree.parse(path)
 
         for info in root.findall('./info'):
-            pkg = factory.begin()
+            with factory.begin() as pkg:
+                nevra = nevra_parse(info.attrib['fn'])
 
-            nevra = nevra_parse(info.attrib['fn'])
+                pkg.set_name(nevra[0])
+                pkg.set_version(nevra[2])
+                pkg.set_rawversion(nevra_construct(None, nevra[1], nevra[2], nevra[3]))
 
-            pkg.set_name(nevra[0])
-            pkg.set_version(nevra[2])
-            pkg.set_rawversion(nevra_construct(None, nevra[1], nevra[2], nevra[3]))
+                # What we do here is we try to extract prerelease part
+                # and mark version as ignored with non-trivial ROSAREV,
+                # as it it likely a snapshot and trus cannot be trusted
+                if not nevra[3].isdecimal():
+                    pkg.set_flags(PackageFlags.ignore)
+                    match = re.search('\\b(a|alpha|b|beta|pre|rc)[0-9]+', nevra[3].lower())
+                    if match:
+                        pkg._package.version += match.group(0)  # XXX: encapsulation violation
 
-            # What we do here is we try to extract prerelease part
-            # and mark version as ignored with non-trivial ROSAREV,
-            # as it it likely a snapshot and trus cannot be trusted
-            if not nevra[3].isdecimal():
-                pkg.set_flags(PackageFlags.ignore)
-                match = re.search('\\b(a|alpha|b|beta|pre|rc)[0-9]+', nevra[3].lower())
-                if match:
-                    pkg.package.version += match.group(0)  # XXX: encapsulation violation
+                pkg.add_homepages(info.attrib['url'])
+                pkg.add_licenses(info.attrib['license'])
 
-            pkg.add_homepages(info.attrib['url'])
-            pkg.add_licenses(info.attrib['license'])
-
-            yield pkg
+                yield pkg
