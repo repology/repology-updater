be0b385b65eca0eba7ef2a75ff1c76e74a2368bb
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-04-30 18:26:50 +0300

    Add parser for ELPA archive-contents s-expr format (#841)

---
+++ repology/parsers/parsers/elpa.py
@@ -0,0 +1,90 @@
+# Copyright (C) 2019 Dmitry Marakasov <amdmi3@amdmi3.ru>
+#
+# This file is part of repology
+#
+# repology is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# repology is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with repology.  If not, see <http://www.gnu.org/licenses/>.
+
+from dataclasses import dataclass
+from typing import Any, Dict, Generator, List
+
+from pyparsing import OneOrMore, QuotedString, Regex, Suppress, Word, ZeroOrMore, alphas, printables
+
+from repology.packagemaker import PackageFactory, PackageMaker
+from repology.parsers import Parser
+from repology.parsers.maintainers import extract_maintainers
+from repology.transformer import PackageTransformer
+
+
+@dataclass
+class _PackageData:
+    name: str
+    version: str
+    summary: str
+    keyvals: Dict[str, Any]
+
+
+def _parse_data(data: str) -> List[_PackageData]:
+    lpar, rpar, lbrk, rbrk, dot = map(Suppress, '()[].')
+    nil = Suppress('nil')
+
+    pkgname = Word(printables)
+    decimal = Regex(r'0|-?[1-9]\d*').setParseAction(lambda t: int(t[0]))
+    qstring = QuotedString(quoteChar='"', escChar='\\')
+
+    version = (lpar + OneOrMore(decimal) + rpar).setParseAction(lambda s, l, t: ['.'.join(map(str, t))])
+
+    dependency_entry = lpar + pkgname + version + rpar
+    dependency_list = ((lpar + OneOrMore(dependency_entry) + rpar) | nil)
+
+    people_list = OneOrMore(qstring | dot | nil)
+
+    keyval_url = (lpar + Suppress(':url') + dot + qstring + rpar).setParseAction(lambda s, l, t: [('url', t[0])])
+    keyval_keywords = (lpar + Suppress(':keywords') + ZeroOrMore(qstring) + rpar).setParseAction(lambda s, l, t: [('keywords', [str(k) for k in t])])
+    keyval_commit = (lpar + Suppress(':commit') + dot + qstring + rpar).setParseAction(lambda s, l, t: [('commit', t[0])])
+    keyval_maintainer = (lpar + Suppress(':maintainer') + people_list + rpar).setParseAction(lambda s, l, t: [('maintainer', [str(m) for m in t])])
+    keyval_author = (lpar + Suppress(':author') + people_list + rpar).setParseAction(lambda s, l, t: [('author', [str(a) for a in t])])
+    keyval_authors = (lpar + Suppress(':authors') + OneOrMore(lpar + people_list + rpar) + rpar).setParseAction(lambda s, l, t: [('authors', [str(a) for a in t])])
+
+    keyval_item = keyval_url | keyval_keywords | keyval_commit | keyval_maintainer | keyval_authors | keyval_author
+
+    keyvals = (lpar + ZeroOrMore(keyval_item) + rpar).setParseAction(lambda s, l, t: [{k: v for k, v in t}]) | nil.setParseAction(lambda s, l, t: [{}])
+
+    package_entry = (lpar + pkgname + dot + lbrk + version + Suppress(dependency_list) + qstring + Suppress(Word(alphas)) + keyvals + rbrk + rpar).setParseAction(lambda s, l, t: [_PackageData(*t)])
+
+    root = lpar + Suppress(decimal) + ZeroOrMore(package_entry) + rpar
+
+    return root.parseString(data)  # type: ignore
+
+
+class ArchiveContentsParser(Parser):
+    def iter_parse(self, path: str, factory: PackageFactory, transformer: PackageTransformer) -> Generator[PackageMaker, None, None]:
+        with open(path, encoding='utf-8', errors='ignore') as contents:
+            data = contents.read()
+
+        for pkgdata in _parse_data(data):
+            with factory.begin() as pkg:
+                pkg.set_name(pkgdata.name)
+                pkg.set_version(pkgdata.version)
+                pkg.set_summary(pkgdata.summary)
+
+                if 'maintainer' in pkgdata.keyvals:
+                    maintainers = sum(map(extract_maintainers, pkgdata.keyvals['maintainer']), [])
+                    pkg.add_maintainers(maintainers)
+
+                pkg.add_homepages(pkgdata.keyvals.get('url'))
+
+                # XXX: enable when we support multiple categories
+                #pkg.add_categories(pkgdata.keyvals.get('keywords'))
+
+                yield pkg
