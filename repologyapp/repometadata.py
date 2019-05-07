6ad701e119a56a26e339d6c1c2048ea313e96309
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-05-07 20:41:27 +0300

    Add typing to RepositoryMetadata

--- repologyapp/repometadata.py
+++ repologyapp/repometadata.py
@@ -16,8 +16,10 @@
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
+from typing import Any, ClassVar, Dict, List, Optional
 
 from repologyapp.db import get_db
+
 
 __all__ = [
     'RepositoryMetadata'
@@ -25,41 +27,45 @@ __all__ = [
 
 
 class RepositoryMetadata:
-    AUTOREFRESH_PERIOD = datetime.timedelta(seconds=300)
+    AUTOREFRESH_PERIOD: ClassVar[datetime.timedelta] = datetime.timedelta(seconds=300)
 
-    def __init__(self):
-        self.repos = []
-        self.by_name = {}
-        self.update_time = None
+    _repos: List[Dict[str, Any]]
+    _by_name: Dict[str, Dict[str, Any]]
+    _update_time: Optional[datetime.datetime]
 
-    def update(self):
-        self.repos = get_db().get_repositories_metadata()  # already sorted by sortname
-        self.by_name = {repo['name']: repo for repo in self.repos}
-        self.update_time = datetime.datetime.now()
+    def __init__(self) -> None:
+        self._repos = []
+        self._by_name = {}
+        self._update_time = None
 
-    def is_stale(self):
-        return self.update_time is None or datetime.datetime.now() - self.update_time > RepositoryMetadata.AUTOREFRESH_PERIOD
+    def update(self) -> None:
+        self._repos = get_db().get_repositories_metadata()  # already sorted by sortname
+        self._by_name = {repo['name']: repo for repo in self._repos}
+        self._update_time = datetime.datetime.now()
 
-    def __getitem__(self, reponame):
-        if reponame not in self.by_name or self.is_stale():
+    def is_stale(self) -> bool:
+        return self._update_time is None or datetime.datetime.now() - self._update_time > RepositoryMetadata.AUTOREFRESH_PERIOD
+
+    def __getitem__(self, reponame: str) -> Dict[str, Any]:
+        if reponame not in self._by_name or self.is_stale():
             self.update()
 
-            if reponame not in self.by_name:
+            if reponame not in self._by_name:
                 raise KeyError('No metadata for repository ' + reponame)
 
-        return self.by_name[reponame]
+        return self._by_name[reponame]
 
-    def __contains__(self, reponame):
+    def __contains__(self, reponame: str) -> bool:
         if self.is_stale():
             self.update()
-        return reponame in self.by_name
+        return reponame in self._by_name
 
-    def all_names(self):
+    def all_names(self) -> List[str]:
         if self.is_stale():
             self.update()
-        return [repo['name'] for repo in self.repos]
+        return [repo['name'] for repo in self._repos]
 
-    def active_names(self):
+    def active_names(self) -> List[str]:
         if self.is_stale():
             self.update()
-        return [repo['name'] for repo in self.repos if repo['active']]
+        return [repo['name'] for repo in self._repos if repo['active']]
