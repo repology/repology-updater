37c0890a80b98579aa0d83d394aa75f15d6ad4e7
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-05-07 21:36:06 +0300

    Add typing for GraphProcessor

--- repologyapp/graphprocessor.py
+++ repologyapp/graphprocessor.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2017 Dmitry Marakasov <amdmi3@amdmi3.ru>
+# Copyright (C) 2016-2019 Dmitry Marakasov <amdmi3@amdmi3.ru>
 #
 # This file is part of repology
 #
@@ -15,39 +15,46 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
+import datetime
 import math
+from typing import List, Optional, Tuple
 
 
 class GraphProcessor:
-    def __init__(self):
-        self.minval = None
-        self.maxval = None
-        self.points = []
-        self.float = False
+    _minval: Optional[float]
+    _maxval: Optional[float]
+    _points: List[Tuple[datetime.timedelta, float]]
+    _float: bool
 
-    def add_point(self, time, value):
+    def __init__(self) -> None:
+        self._minval = None
+        self._maxval = None
+        self._points = []
+        self._float = False
+
+    def add_point(self, time: datetime.timedelta, value: float) -> None:
         # minor optimization of merging straight lines
-        if len(self.points) >= 2 and value == self.points[-1][1] and value == self.points[-2][1]:
-            del(self.points[-1])
+        if len(self._points) >= 2 and value == self._points[-1][1] and value == self._points[-2][1]:
+            del(self._points[-1])
 
-        self.minval = value if self.minval is None else min(value, self.minval)
-        self.maxval = value if self.maxval is None else max(value, self.maxval)
-        self.points.append([time, value])
+        self._minval = value if self._minval is None else min(value, self._minval)
+        self._maxval = value if self._maxval is None else max(value, self._maxval)
+        self._points.append((time, value))
         if isinstance(value, float):
-            self.float = True
+            self._float = True
 
-    def get_points(self, period):
-        if self.minval is None:
+    def get_points(self, period: int) -> List[Tuple[float, float]]:
+        if self._minval is None or self._maxval is None:
             return []
 
-        if self.minval == self.maxval:
+        if self._minval == self._maxval:
             return [
                 (
-                    self.points[0][0].total_seconds() / period,
+                    self._points[0][0].total_seconds() / period,
                     0.5
                 ),
                 (
-                    self.points[-1][0].total_seconds() / period,
+                    self._points[-1][0].total_seconds() / period,
                     0.5
                 )
             ]
@@ -55,25 +62,25 @@ class GraphProcessor:
         return [
             (
                 point[0].total_seconds() / period,
-                (point[1] - self.minval) / (self.maxval - self.minval)
-            ) for point in self.points
+                (point[1] - self._minval) / (self._maxval - self._minval)
+            ) for point in self._points
         ]
 
-    def get_y_ticks(self, suffix=''):
-        if self.minval is None:
+    def get_y_ticks(self, suffix: str = '') -> List[Tuple[float, str]]:
+        if self._minval is None or self._maxval is None:
             return []
 
-        if self.minval == self.maxval:
-            value = self.points[0][1]
+        if self._minval == self._maxval:
+            value = self._points[0][1]
 
             rounding = 0 if isinstance(value, int) else 3
 
             return [(0.5, '{:.{}f}{}'.format(value, rounding, suffix))]
 
-        step = (self.maxval - self.minval) / 10
+        step = (self._maxval - self._minval) / 10
 
-        steps = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]
-        if self.float:
+        steps: List[float] = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]
+        if self._float:
             steps = [0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5] + steps
 
         for roundedstep in steps:
@@ -89,12 +96,12 @@ class GraphProcessor:
         elif step < 1:
             rounding = 1
 
-        lowtick = math.ceil(self.minval / step) * step
-        numticks = int((self.maxval - lowtick) / step) + 1
+        lowtick = math.ceil(self._minval / step) * step
+        numticks = int((self._maxval - lowtick) / step) + 1
 
         return [
             (
-                (lowtick + step * n - self.minval) / (self.maxval - self.minval),
+                (lowtick + step * n - self._minval) / (self._maxval - self._minval),
                 '{:.{}f}{}'.format(lowtick + step * n, rounding, suffix)
             ) for n in range(numticks)
         ]
