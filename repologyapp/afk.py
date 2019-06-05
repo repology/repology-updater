a8d54a5966210131aabaf2c8be15dfc456c9a871
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-06-06 01:04:34 +0300

    Typing for AFKChecker

--- repologyapp/afk.py
+++ repologyapp/afk.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2017 Dmitry Marakasov <amdmi3@amdmi3.ru>
+# Copyright (C) 2016-2019 Dmitry Marakasov <amdmi3@amdmi3.ru>
 #
 # This file is part of repology
 #
@@ -16,24 +16,28 @@
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
 import datetime
+from typing import List, Optional, Tuple
 
 
 class AFKChecker:
-    def __init__(self, intervals=[]):
-        self.intervals = []
+    _intervals: List[Tuple[datetime.date, datetime.date]]
+
+    def __init__(self, intervals: List[str] = []) -> None:
+        self._intervals = []
+
         for interval in intervals:
             start, *rest = [
                 datetime.date(*map(int, date.split('-', 2)))
                 for date in interval.split(' ', 1)
             ]
 
-            self.intervals.append((start, rest[0] if rest else start))
+            self._intervals.append((start, rest[0] if rest else start))
 
-    def get_afk_end(self, today=None):
+    def get_afk_end(self, today: Optional[datetime.date] = None) -> Optional[datetime.date]:
         if today is None:
             today = datetime.date.today()
 
-        for interval in self.intervals:
+        for interval in self._intervals:
             if today >= interval[0] and today <= interval[1]:
                 return interval[1]
 
