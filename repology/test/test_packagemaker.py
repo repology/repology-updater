b4348c1b283ec8bc055ee3612a1a1f6f3dbe8363
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-09-14 01:11:39 +0300

    Refactor PackageMaker test
    Always fill required Package fields and fix typing

--- repology/test/test_packagemaker.py
+++ repology/test/test_packagemaker.py
@@ -17,32 +17,59 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-# mypy: no-disallow-untyped-calls
-
 import unittest
-from typing import Any, Iterator
+from typing import Any, Iterator, Optional
 
-from repology.logger import AccumulatingLogger, NoopLogger
-from repology.packagemaker import PackageFactory
+from repology.logger import AccumulatingLogger, Logger, NoopLogger
+from repology.package import Package
+from repology.packagemaker import PackageFactory, PackageMaker
+
+
+class TestPackage:
+    _factory: PackageFactory
+    _package_maker: Optional[PackageMaker]
+    _package: Optional[Package]
+
+    def __init__(self, logger: Logger = NoopLogger()) -> None:
+        self._factory = PackageFactory(logger)
+        self._package_maker = None
+        self._package = None
+
+    def __enter__(self) -> PackageMaker:
+        self._package_maker = self._factory.begin()
+        return self._package_maker
+
+    def __exit__(self, *rest: Any) -> None:
+        assert(self._package_maker)
+
+        if self._package_maker.name is None:
+            self._package_maker.set_name('dummy_package')
+        if self._package_maker.version is None:
+            self._package_maker.set_version('0dummy0')
+
+        self._package = self._package_maker.spawn('dummy_repo', 'dummy_family')
+
+    def __getattr__(self, key: str) -> Any:
+        assert(self._package)
+        return getattr(self._package, key)
 
 
 class TestPackageMaker(unittest.TestCase):
     def test_all_fields(self) -> None:
-        factory = PackageFactory(NoopLogger())
+        pkg = TestPackage()
 
-        maker = factory.begin()
-        maker.set_name_and_version('foo-1.0')
-        maker.set_origin('/foo')
-        maker.set_summary('foo package')
-        maker.add_maintainers(None, 'a@com', [None, ['b@com']], None, 'c@com')
-        maker.add_maintainers('d@com')
-        maker.add_categories(None, 'foo', 'bar')
-        maker.add_categories('baz')
-        maker.add_homepages('http://foo/', 'http://bar/')
-        maker.add_licenses(['GPLv2', 'GPLv3'])
-        maker.add_licenses('MIT')
-        maker.add_downloads(None, [None, 'http://baz/'], ['ftp://quux/'])
-        pkg = maker.unwrap()
+        with pkg as maker:
+            maker.set_name_and_version('foo-1.0')
+            maker.set_origin('/foo')
+            maker.set_summary('foo package')
+            maker.add_maintainers(None, 'a@com', [None, ['b@com']], None, 'c@com')
+            maker.add_maintainers('d@com')
+            maker.add_categories(None, 'foo', 'bar')
+            maker.add_categories('baz')
+            maker.add_homepages('http://foo/', 'http://bar/')
+            maker.add_licenses(['GPLv2', 'GPLv3'])
+            maker.add_licenses('MIT')
+            maker.add_downloads(None, [None, 'http://baz/'], ['ftp://quux/'])
 
         self.assertEqual(pkg.name, 'foo')
         self.assertEqual(pkg.version, '1.0')
@@ -55,101 +82,93 @@ class TestPackageMaker(unittest.TestCase):
 
     def test_validate_urls(self) -> None:
         logger = AccumulatingLogger()
-        factory = PackageFactory(logger)
+        pkg = TestPackage(logger)
 
-        maker = factory.begin()
-        maker.add_downloads('http://www.valid/', 'https://www.valid/some', 'ftp://ftp.valid/', 'invalid')
-        pkg = maker.unwrap()
+        with pkg as maker:
+            maker.add_downloads('http://www.valid/', 'https://www.valid/some', 'ftp://ftp.valid/', 'invalid')
 
         self.assertEqual(pkg.downloads, ['http://www.valid/', 'https://www.valid/some', 'ftp://ftp.valid/'])
         self.assertEqual(len(logger.get()), 1)
         self.assertTrue('invalid' in logger.get()[0])
 
     def test_normalize_urls(self) -> None:
-        factory = PackageFactory(NoopLogger())
+        pkg = TestPackage()
 
-        maker = factory.begin()
-        maker.add_homepages('Http://Foo.coM')
-        maker.add_downloads('Http://Foo.coM')
-        pkg = maker.unwrap()
+        with pkg as maker:
+            maker.add_homepages('Http://Foo.coM')
+            maker.add_downloads('Http://Foo.coM')
 
         self.assertEqual(pkg.homepage, 'http://foo.com/')
         self.assertEqual(pkg.downloads, ['http://foo.com/'])
 
     def test_unicalization_with_order_preserved(self) -> None:
-        factory = PackageFactory(NoopLogger())
+        pkg = TestPackage()
 
-        maker = factory.begin()
-        maker.add_maintainers('z@com', 'y@com', 'x@com', 'z@com', 'y@com', 'x@com')
-        maker.add_maintainers('z@com', 'y@com', 'x@com')
-        pkg = maker.unwrap()
+        with pkg as maker:
+            maker.add_maintainers('z@com', 'y@com', 'x@com', 'z@com', 'y@com', 'x@com')
+            maker.add_maintainers('z@com', 'y@com', 'x@com')
 
         self.assertEqual(pkg.maintainers, ['z@com', 'y@com', 'x@com'])
 
     def test_strip(self) -> None:
-        factory = PackageFactory(NoopLogger())
+        pkg = TestPackage()
 
-        maker = factory.begin()
-        maker.set_summary('       some package foo      ')
-        pkg = maker.unwrap()
+        with pkg as maker:
+            maker.set_summary('       some package foo      ')
 
         self.assertEqual(pkg.comment, 'some package foo')
 
     def test_redefine(self) -> None:
-        factory = PackageFactory(NoopLogger())
+        pkg = TestPackage()
 
-        maker = factory.begin()
-        maker.set_name('foo')
-        maker.set_name('bar')
-        maker.set_version('1.0')
-        maker.set_version('1.1')
-        maker.set_summary('Foo')
-        maker.set_summary('Bar')
-        pkg = maker.unwrap()
+        with pkg as maker:
+            maker.set_name('foo')
+            maker.set_name('bar')
+            maker.set_version('1.0')
+            maker.set_version('1.1')
+            maker.set_summary('Foo')
+            maker.set_summary('Bar')
 
         self.assertEqual(pkg.name, 'bar')
         self.assertEqual(pkg.version, '1.1')
         self.assertEqual(pkg.comment, 'Bar')
 
     def test_type_normalization1(self) -> None:
-        factory = PackageFactory(NoopLogger())
+        pkg = TestPackage()
 
-        maker = factory.begin()
-        maker.set_name(0)
-        maker.set_version(0)
-        maker.set_summary(0)
-        pkg = maker.unwrap()
+        with pkg as maker:
+            maker.set_name(0)
+            maker.set_version(0)
+            maker.set_summary(0)
 
         self.assertEqual(pkg.name, '0')
         self.assertEqual(pkg.version, '0')
         self.assertEqual(pkg.comment, '0')
 
     def test_type_normalization2(self) -> None:
-        factory = PackageFactory(NoopLogger())
+        pkg = TestPackage()
 
-        maker = factory.begin()
-
-        with self.assertRaises(RuntimeError):
-            maker.set_name([123])
-        with self.assertRaises(RuntimeError):
-            maker.set_version([123])
-        with self.assertRaises(RuntimeError):
-            maker.set_summary([123])
+        with pkg as maker:
+            with self.assertRaises(RuntimeError):
+                maker.set_name([123])
+            with self.assertRaises(RuntimeError):
+                maker.set_version([123])
+            with self.assertRaises(RuntimeError):
+                maker.set_summary([123])
 
     def test_nulls(self) -> None:
-        factory = PackageFactory(NoopLogger())
+        pkg = TestPackage()
 
-        maker = factory.begin()
-        maker.set_name('foo')
-        maker.set_name('')
-        maker.set_name(None)
-        maker.set_version('1.0')
-        maker.set_version('')
-        maker.set_version(None)
-        maker.set_summary('Foo')
-        maker.set_summary('')
-        maker.set_summary(None)
-        pkg = maker.unwrap()
+        with pkg as maker:
+            maker.set_name('foo')
+            maker.set_name('')
+            maker.set_name(None)
+            maker.set_version('1.0')
+            maker.set_version('')
+            maker.set_version(None)
+            maker.set_summary('Foo')
+            maker.set_summary('')
+            maker.set_summary(None)
 
         self.assertEqual(pkg.name, 'foo')
         self.assertEqual(pkg.version, '1.0')
@@ -163,11 +182,10 @@ class TestPackageMaker(unittest.TestCase):
             yield ''
             yield 'd@com'
 
-        factory = PackageFactory(NoopLogger())
+        pkg = TestPackage()
 
-        maker = factory.begin()
-        maker.add_maintainers(iter_maintainers())
-        pkg = maker.unwrap()
+        with pkg as maker:
+            maker.add_maintainers(iter_maintainers())
 
         self.assertEqual(pkg.maintainers, ['a@com', 'b@com', 'c@com', 'd@com'])
 
