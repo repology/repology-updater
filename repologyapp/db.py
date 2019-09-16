90ed61b06d9491f561be04e6b2393f2ca31b2b50
Dmitry Marakasov <amdmi3@amdmi3.ru>
2019-09-16 23:38:33 +0300

    Untie webapp from repology.database

--- repologyapp/db.py
+++ repologyapp/db.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2018 Dmitry Marakasov <amdmi3@amdmi3.ru>
+# Copyright (C) 2016-2019 Dmitry Marakasov <amdmi3@amdmi3.ru>
 #
 # This file is part of repology
 #
@@ -15,11 +15,14 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
+from typing import Any, Optional, TYPE_CHECKING
+
 import flask
+
+import psycopg2
 
 from repologyapp.config import config
 
-from repology.database import Database
 from repology.querymgr import QueryManager
 
 
@@ -29,6 +32,24 @@ __all__ = [
 
 
 _querymgr = QueryManager(config['SQL_DIR'])
+
+
+class Database:
+    _db: Any  # no typing support for psycopg
+
+    def __init__(self, dsn: str, querymgr: QueryManager, readonly: bool = True, autocommit: bool = False, application_name: Optional[str] = None) -> None:
+        self._db = psycopg2.connect(dsn, application_name=application_name)
+        self._db.set_session(readonly=readonly, autocommit=autocommit)
+        querymgr.inject_queries(self, self._db)
+
+    def commit(self) -> None:
+        self._db.commit()
+
+    # this class is filled by methods by querymgr
+    # mypy doesn't know about them so we have to silence it this way
+    if TYPE_CHECKING:
+        def __getattr__(self, name: str) -> Any:
+            pass
 
 
 def get_db() -> Database:
