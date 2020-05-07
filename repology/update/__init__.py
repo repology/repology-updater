8a0f57c54481d36b78978b09194ee8707c9f61db
Dmitry Marakasov <amdmi3@amdmi3.ru>
2020-05-07 20:05:12 +0300

    Improve log message

--- repology/update/__init__.py
+++ repology/update/__init__.py
@@ -64,7 +64,7 @@ class UpdateProcess:
         self._logger.log('starting the update')
         self._database.update_start()
 
-        self._logger.log('forcing updates for projects affected by changed CVEs')
+        self._logger.log('forcing update for projects affected by changed CVEs')
         self._database.update_force_project_updates_by_cve()
 
     def _push_packages(self, projects: Iterable[List[Package]]) -> None:
