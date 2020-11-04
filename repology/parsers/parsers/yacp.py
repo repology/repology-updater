162b750ee4096caf307624ab75436002d515ab0f
Dmitry Marakasov <amdmi3@amdmi3.ru>
2020-11-04 20:16:14 +0300

    Switch to new YACP package metadata source (fixes #1093)

--- repology/parsers/parsers/yacp.py
+++ repology/parsers/parsers/yacp.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2019 Dmitry Marakasov <amdmi3@amdmi3.ru>
+# Copyright (C) 2016-2020 Dmitry Marakasov <amdmi3@amdmi3.ru>
 #
 # This file is part of repology
 #
@@ -15,55 +15,22 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-import re
-from typing import Iterable, Tuple
+from typing import Iterable
 
 from repology.packagemaker import NameType, PackageFactory, PackageMaker
 from repology.parsers import Parser
-from repology.parsers.versions import VersionStripper
+from repology.parsers.json import iter_json_list
 from repology.transformer import PackageTransformer
 
 
-def _iter_cygports(path: str) -> Iterable[Tuple[str, str]]:
-    for package in os.listdir(path):
-        package_path = os.path.join(path, package)
-        if not os.path.isdir(package_path):
-            continue
-
-        for cygport in os.listdir(package_path):
-            if not cygport.endswith('.cygport'):
-                continue
-
-            yield os.path.join(package_path, cygport), cygport
-
-
-class YACPGitParser(Parser):
+class YACPJsonParser(Parser):
     def iter_parse(self, path: str, factory: PackageFactory, transformer: PackageTransformer) -> Iterable[PackageMaker]:
-        normalize_version = VersionStripper().strip_right('+')
-
-        for cygport_path, cygport_name in _iter_cygports(path):
-            pkg = factory.begin(cygport_name)
-
-            # XXX: save *bl* to rawversion
-            match = re.match('(.*)-[0-9]+bl[0-9]+\\.cygport$', cygport_name)
-            if not match:
-                raise RuntimeError('unable to parse cygport name')
-
-            name, version = match.group(1).rsplit('-', 1)
-
-            pkg.add_name(name, NameType.YACP_NAME)
-            pkg.set_version(version, normalize_version)
-
-            # these fields do not contain variables (for now), so are safe to extract
-            with open(cygport_path, 'r') as cygdata:
-                for line in cygdata:
-                    match = re.match('CATEGORY="([^"$]+)"', line)
-                    if match:
-                        pkg.add_categories(match.group(1))
-
-                    match = re.match('SUMMARY="([^"$]+)"', line)
-                    if match:
-                        pkg.set_summary(match.group(1))
-
-            yield pkg
+        for pkgdata in iter_json_list(path, ('packages', None)):
+            with factory.begin() as pkg:
+                pkg.add_name(pkgdata['name'], NameType.YACP_NAME)
+                pkg.set_version(pkgdata['version'])
+                pkg.add_categories(pkgdata['category'])
+                pkg.set_summary(pkgdata['summary'])
+                pkg.add_homepages(pkgdata['homepage'])
+                pkg.add_binnames(subpackage['name'] for subpackage in pkgdata['subpackages'])
+                yield pkg
