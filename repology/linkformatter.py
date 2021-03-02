ec4ff71e1f7af5c8df41420785a95fd569c0c20f
Dmitry Marakasov <amdmi3@amdmi3.ru>
2021-03-03 01:09:49 +0300

    Implement link formatter (#1129)
    This is based on (and will replace) PackageFormatter from
    repology-webapp, with some additional features:
    - support for non-string values
    - support for lists of values; each value in a list produces another
      link from a template, and multiple list lead to a cartesian product
    - better error handling which is not possible in the webapp

---
+++ repology/linkformatter.py
@@ -0,0 +1,122 @@
+# Copyright (C) 2016-2021 Dmitry Marakasov <amdmi3@amdmi3.ru>
+#
+# This file is part of repology
+#
+# repology is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# repology is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with repology.  If not, see <http://www.gnu.org/licenses/>.
+
+import urllib.parse
+from itertools import product
+from typing import Any, Callable, Dict, Iterator
+
+from repology.package import Package
+
+__all__ = ['format_package_links']
+
+
+_filters: Dict[str, Callable[[str], str]] = {
+    'lowercase': lambda x: x.lower(),
+    'firstletter': lambda x: x.lower()[0],
+    'libfirstletter': lambda x: x.lower()[:4] if x.lower().startswith('lib') else x.lower()[0],
+    'stripdmo': lambda x: x[:-4] if x.endswith('-dmo') else x,
+    'basename': lambda x: x.rsplit('/', 1)[-1],
+    'dirname': lambda x: x.rsplit('/', 1)[0],
+    'dec': lambda x: str(_safe_int(x) - 1),
+    'inc': lambda x: str(_safe_int(x) + 1),
+}
+
+
+def _safe_int(arg: str) -> int:
+    try:
+        return int(arg)
+    except ValueError:
+        return 0
+
+
+class FieldGatheringMapping:
+    _package: Package
+    _fields: Dict[str, Any]
+
+    def __init__(self, package: Package) -> None:
+        self._package = package
+        self._fields = {}
+
+    def __getitem__(self, key: str) -> str:
+        field, *filters = key.split('|', 1)
+
+        if field in self._fields:
+            return ''
+
+        value: Any = None
+
+        if field == 'name':
+            value = self._package.name
+        elif field == 'srcname':
+            value = self._package.srcname
+        elif field == 'binname':
+            value = self._package.binname
+        elif field == 'subrepo':
+            value = self._package.subrepo
+        elif field == 'version':
+            value = self._package.version
+        elif field == 'origversion':
+            value = self._package.origversion
+        elif field == 'rawversion':
+            value = self._package.rawversion
+        elif field == 'category':
+            value = self._package.category or ''
+        elif field == 'archrepo':
+            # XXX: switch this to per-source packagelinks in config
+            if self._package.subrepo and self._package.subrepo.startswith('community'):
+                value = 'community'
+            else:
+                value = 'packages'
+        elif self._package.extrafields is not None and field in self._package.extrafields:
+            value = self._package.extrafields[field]
+
+        if not value:
+            raise RuntimeError(f'missing key "{field}"')
+        elif isinstance(value, list):
+            if filters:
+                raise RuntimeError(f'cannot apply filter to list of values in "{field}"')
+            self._fields[key] = value
+        elif isinstance(value, str):
+            for filtername in filters:
+                if filtername not in _filters:
+                    raise RuntimeError(f'unknown filter "filtername" in "{field}"')
+                value = _filters[filtername](value)
+
+            value = urllib.parse.quote(value)
+
+            self._fields[key] = [value]
+        else:
+            if filters:
+                raise RuntimeError(f'cannot apply filter to non-string value in "{field}"')
+            self._fields[key] = [value]
+
+        return ''
+
+    def generate_mappings(self) -> Iterator[Dict[str, Any]]:
+        keys = list(self._fields.keys())
+
+        for values in product(*self._fields.values()):
+            yield dict(zip(keys, values))
+
+
+def format_package_links(package: Package, link_template: str) -> Iterator[str]:
+    field_gatherer = FieldGatheringMapping(package)
+
+    link_template.format_map(field_gatherer)
+
+    for mapping in field_gatherer.generate_mappings():
+        yield link_template.format_map(mapping)
---
+++ repology/test/test_linkformatter.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2021 Dmitry Marakasov <amdmi3@amdmi3.ru>
+#
+# This file is part of repology
+#
+# repology is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# repology is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with repology.  If not, see <http://www.gnu.org/licenses/>.
+
+# mypy: no-disallow-untyped-calls
+
+import unittest
+
+from repology.linkformatter import format_package_links
+
+from .package import spawn_package
+
+
+class TestLinkFormatter(unittest.TestCase):
+    def test_basic(self) -> None:
+        self.assertEqual(
+            list(
+                format_package_links(
+                    spawn_package(
+                        name='foo',
+                        version='1.1',
+                    ),
+                    'https://example.com/{name}/{version}'
+                )
+            ),
+            [
+                'https://example.com/foo/1.1'
+            ]
+        )
+
+    def test_filter(self) -> None:
+        self.assertEqual(
+            list(
+                format_package_links(
+                    spawn_package(
+                        name='FOO',
+                        version='1.1',
+                    ),
+                    'https://example.com/{name|lowercase}'
+                )
+            ),
+            [
+                'https://example.com/foo'
+            ]
+        )
+
+    def test_list(self) -> None:
+        package = spawn_package()
+        package.extrafields = {'patch': ['foo', 'bar', 'baz'], 'suffix': ['a', 'b']}
+
+        self.assertEqual(
+            list(
+                format_package_links(package, 'https://example.com/{patch}{suffix}')
+            ),
+            [
+                'https://example.com/fooa',
+                'https://example.com/foob',
+                'https://example.com/bara',
+                'https://example.com/barb',
+                'https://example.com/baza',
+                'https://example.com/bazb',
+            ]
+        )
+
+    def test_raises(self) -> None:
+        package = spawn_package()
+
+        with self.assertRaises(RuntimeError):
+            list(format_package_links(package, 'https://example.com/{unknown}'))
+
+        with self.assertRaises(RuntimeError):
+            list(format_package_links(package, 'https://example.com/{name|unknownfilter}'))
+
+
+if __name__ == '__main__':
+    unittest.main()
