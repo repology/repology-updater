3a8cf2df4f72ba64ad2438e73b2ddc566adb9520
Dmitry Marakasov <amdmi3@amdmi3.ru>
2021-03-03 15:33:41 +0300

    Switch kiss to generated packagelinks, including patches (#1129)

--- repology/parsers/parsers/kiss.py
+++ repology/parsers/parsers/kiss.py
@@ -19,7 +19,6 @@ import os
 import subprocess
 from typing import Iterable
 
-from repology.package import LinkType
 from repology.packagemaker import NameType, PackageFactory, PackageMaker
 from repology.parsers import Parser
 from repology.parsers.maintainers import extract_maintainers
@@ -29,11 +28,9 @@ from repology.transformer import PackageTransformer
 
 class KissGitParser(Parser):
     _maintainer_from_git: bool
-    _blob_prefix: str
 
-    def __init__(self, blob_prefix: str, maintainer_from_git: bool = False):
+    def __init__(self, maintainer_from_git: bool = False):
         self._maintainer_from_git = maintainer_from_git
-        self._blob_prefix = blob_prefix
 
     def iter_parse(self, path: str, factory: PackageFactory, transformer: PackageTransformer) -> Iterable[PackageMaker]:
         for versionpath in walk_tree(path, name='version'):
@@ -82,11 +79,6 @@ class KissGitParser(Parser):
                     with open(os.path.join(rootdir, 'build')) as fd:
                         build_script = fd.read()
 
-                    for patch in patches:
-                        if patch in build_script:
-                            pkg.add_links(
-                                LinkType.PACKAGE_PATCH,
-                                f'{self._blob_prefix}{pkgpath}/patches/{patch}'
-                            )
+                    pkg.set_extra_field('patch', [patch for patch in patches if patch in build_script])
 
                 yield pkg
