583519f03537aa7be5d10df1540a8e118eaa398f
jhan <bluebottle.new@gmail.com>
2021-04-13 19:35:24 +0200

    Fixing expression

--- repology/parsers/cpe.py
+++ repology/parsers/cpe.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-def cpe_parse(cpe_str: str) -> []:
+def cpe_parse(cpe_str: str) -> list[...]:
     escaped = False
     current = ''
     res = []
