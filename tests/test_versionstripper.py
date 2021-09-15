7276496eadb2ed35486481394178ec09d00a7dba
Dmitry Marakasov <amdmi3@amdmi3.ru>
2021-09-15 17:00:24 +0300

    Convert test_versionstripper to pytest

--- tests/test_versionstripper.py
+++ tests/test_versionstripper.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-#
-# Copyright (C) 2018 Dmitry Marakasov <amdmi3@amdmi3.ru>
+# Copyright (C) 2018-2019,2021 Dmitry Marakasov <amdmi3@amdmi3.ru>
 #
 # This file is part of repology
 #
@@ -17,25 +15,20 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-import unittest
-
 from repology.parsers.versions import VersionStripper
 
 
-class TestVersionStripper(unittest.TestCase):
-    def test_identity(self) -> None:
-        self.assertEqual(VersionStripper()('1.2.3'), '1.2.3')
-
-    def test_basic(self) -> None:
-        self.assertEqual(VersionStripper().strip_left('.')('1.2.3'), '2.3')
-        self.assertEqual(VersionStripper().strip_left_greedy('.')('1.2.3'), '3')
-        self.assertEqual(VersionStripper().strip_right('.')('1.2.3'), '1.2')
-        self.assertEqual(VersionStripper().strip_right_greedy('.')('1.2.3'), '1')
-
-    def test_order(self) -> None:
-        self.assertEqual(VersionStripper().strip_right('_').strip_right(',')('1_2,3_4'), '1_2')
-        self.assertEqual(VersionStripper().strip_right(',').strip_right('_')('1_2,3_4'), '1')
+def test_identity() -> None:
+    assert VersionStripper()('1.2.3') == '1.2.3'
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_basic() -> None:
+    assert VersionStripper().strip_left('.')('1.2.3') == '2.3'
+    assert VersionStripper().strip_left_greedy('.')('1.2.3') == '3'
+    assert VersionStripper().strip_right('.')('1.2.3') == '1.2'
+    assert VersionStripper().strip_right_greedy('.')('1.2.3') == '1'
+
+
+def test_order() -> None:
+    assert VersionStripper().strip_right('_').strip_right(',')('1_2,3_4') == '1_2'
+    assert VersionStripper().strip_right(',').strip_right('_')('1_2,3_4') == '1'
