b33b04a3ed04c7aa57c3a71d138b1bf805e688f8
Dmitry Marakasov <amdmi3@amdmi3.ru>
2021-09-17 14:39:59 +0300

    Convert another test to pytest

--- tests/test_gentoo_parseexpr.py
+++ tests/test_gentoo_parseexpr.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-#
-# Copyright (C) 2016 Dmitry Marakasov <amdmi3@amdmi3.ru>
+# Copyright (C) 2016-2019,2021 Dmitry Marakasov <amdmi3@amdmi3.ru>
 #
 # This file is part of repology
 #
@@ -17,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-import unittest
 from typing import List
 
 from repology.parsers.parsers.gentoo import _parse_conditional_expr
@@ -27,46 +24,44 @@ def parse_conditional_expr(string: str) -> List[str]:
     return list(_parse_conditional_expr(string))
 
 
-class TestGentooParseExpr(unittest.TestCase):
-    def test_simple(self) -> None:
-        self.assertEqual(parse_conditional_expr('http://foo'), ['http://foo'])
-
-    def test_multiple(self) -> None:
-        self.assertEqual(parse_conditional_expr('http://foo http://bar'), ['http://foo', 'http://bar'])
-
-    def test_rename(self) -> None:
-        self.assertEqual(parse_conditional_expr('http://foo/file.tgz -> file.tar.gz'), ['http://foo/file.tgz'])
-
-    def test_condition(self) -> None:
-        self.assertEqual(parse_conditional_expr('!http? ( http://foo )'), ['http://foo'])
-        self.assertEqual(parse_conditional_expr('!http? ( http://foo ) !ftp? ( http://bar )'), ['http://foo', 'http://bar'])
-
-    def test_nested_condition(self) -> None:
-        self.assertEqual(parse_conditional_expr('!http? ( !ftp? ( http://foo ) )'), ['http://foo'])
-
-    def test_realworld(self) -> None:
-        self.assertEqual(
-            parse_conditional_expr(
-                'mirror://sourceforge/free-doko/FreeDoko_0.7.14.src.zip backgrounds? '
-                '( mirror://sourceforge/free-doko/backgrounds.zip -> freedoko-backgrounds.zip ) '
-                'kdecards? ( mirror://sourceforge/free-doko/kdecarddecks.zip ) xskatcards? '
-                '( mirror://sourceforge/free-doko/xskat.zip ) pysolcards? ( mirror://sourceforge/free-doko/pysol.zip '
-                ') gnomecards? ( mirror://sourceforge/free-doko/gnome-games.zip ) '
-                'openclipartcards? ( mirror://sourceforge/free-doko/openclipart.zip ) '
-                '!xskatcards? ( !kdecards? ( !gnomecards? ( !openclipartcards? ( !pysolcards? '
-                '( mirror://sourceforge/free-doko/xskat.zip ) ) ) ) )',
-            ), [
-                'mirror://sourceforge/free-doko/FreeDoko_0.7.14.src.zip',
-                'mirror://sourceforge/free-doko/backgrounds.zip',
-                'mirror://sourceforge/free-doko/kdecarddecks.zip',
-                'mirror://sourceforge/free-doko/xskat.zip',
-                'mirror://sourceforge/free-doko/pysol.zip',
-                'mirror://sourceforge/free-doko/gnome-games.zip',
-                'mirror://sourceforge/free-doko/openclipart.zip',
-                'mirror://sourceforge/free-doko/xskat.zip'
-            ]
-        )
+def test_simple() -> None:
+    assert parse_conditional_expr('http://foo') == ['http://foo']
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_multiple() -> None:
+    assert parse_conditional_expr('http://foo http://bar') == ['http://foo', 'http://bar']
+
+
+def test_rename() -> None:
+    assert parse_conditional_expr('http://foo/file.tgz -> file.tar.gz') == ['http://foo/file.tgz']
+
+
+def test_condition() -> None:
+    assert parse_conditional_expr('!http? ( http://foo )') == ['http://foo']
+    assert parse_conditional_expr('!http? ( http://foo ) !ftp? ( http://bar )') == ['http://foo', 'http://bar']
+
+
+def test_nested_condition() -> None:
+    assert parse_conditional_expr('!http? ( !ftp? ( http://foo ) )') == ['http://foo']
+
+
+def test_realworld() -> None:
+    assert parse_conditional_expr(
+        'mirror://sourceforge/free-doko/FreeDoko_0.7.14.src.zip backgrounds? '
+        '( mirror://sourceforge/free-doko/backgrounds.zip -> freedoko-backgrounds.zip ) '
+        'kdecards? ( mirror://sourceforge/free-doko/kdecarddecks.zip ) xskatcards? '
+        '( mirror://sourceforge/free-doko/xskat.zip ) pysolcards? ( mirror://sourceforge/free-doko/pysol.zip '
+        ') gnomecards? ( mirror://sourceforge/free-doko/gnome-games.zip ) '
+        'openclipartcards? ( mirror://sourceforge/free-doko/openclipart.zip ) '
+        '!xskatcards? ( !kdecards? ( !gnomecards? ( !openclipartcards? ( !pysolcards? '
+        '( mirror://sourceforge/free-doko/xskat.zip ) ) ) ) )',
+    ) == [
+        'mirror://sourceforge/free-doko/FreeDoko_0.7.14.src.zip',
+        'mirror://sourceforge/free-doko/backgrounds.zip',
+        'mirror://sourceforge/free-doko/kdecarddecks.zip',
+        'mirror://sourceforge/free-doko/xskat.zip',
+        'mirror://sourceforge/free-doko/pysol.zip',
+        'mirror://sourceforge/free-doko/gnome-games.zip',
+        'mirror://sourceforge/free-doko/openclipart.zip',
+        'mirror://sourceforge/free-doko/xskat.zip'
+    ]
