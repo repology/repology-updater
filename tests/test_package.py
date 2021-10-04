7cc55c624fc63f437d699d609ea03eb7379a03d7
Dmitry Marakasov <amdmi3@amdmi3.ru>
2021-10-04 14:54:53 +0300

    Convert a test to pytest

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-#
-# Copyright (C) 2016 Dmitry Marakasov <amdmi3@amdmi3.ru>
+# Copyright (C) 2016-2021 Dmitry Marakasov <amdmi3@amdmi3.ru>
 #
 # This file is part of repology
 #
@@ -19,151 +17,125 @@
 
 # mypy: no-disallow-untyped-calls
 
-import unittest
-
 from repology.package import LinkType, Package, PackageFlags
 
 from .package import spawn_package
 
 
-class TestParsers(unittest.TestCase):
-    def assert_version_compare(self, a: Package, b: Package, res: int) -> None:
-        self.assertEqual(a.version_compare(b), res)
-
-    def test_equality(self) -> None:
-        self.assertEqual(
-            spawn_package(),
-            spawn_package()
-        )
-        self.assertEqual(
-            spawn_package(name='a'),
-            spawn_package(name='a')
-        )
-        self.assertEqual(
-            spawn_package(
-                links=[
-                    (LinkType.UPSTREAM_HOMEPAGE, 'http://foo'),
-                    (LinkType.UPSTREAM_HOMEPAGE, 'http://foo'),
-                    (LinkType.UPSTREAM_HOMEPAGE, 'http://bar'),
-                    (LinkType.UPSTREAM_HOMEPAGE, 'http://bar'),
-                ]
-            ),
-            spawn_package(
-                links=[
-                    (LinkType.UPSTREAM_HOMEPAGE, 'http://foo'),
-                    (LinkType.UPSTREAM_HOMEPAGE, 'http://bar'),
-                ]
-            )
-        )
-
-        self.assertNotEqual(
-            spawn_package(name='a'),
-            spawn_package(name='b')
-        )
-        self.assertNotEqual(
-            spawn_package(),
-            spawn_package(name='b')
-        )
-        self.assertNotEqual(
-            spawn_package(name='a'),
-            spawn_package()
-        )
-
-    def test_flag_p_is_patch(self) -> None:
-        self.assert_version_compare(
-            spawn_package(version='1.0p1'),
-            spawn_package(version='1.0p1'),
-            0
-        )
-        self.assert_version_compare(
-            spawn_package(version='1.0p1', flags=PackageFlags.P_IS_PATCH),
-            spawn_package(version='1.0p1'),
-            1
-        )
-        self.assert_version_compare(
-            spawn_package(version='1.0p1'),
-            spawn_package(version='1.0p1', flags=PackageFlags.P_IS_PATCH),
-            -1
-        )
-        self.assert_version_compare(
-            spawn_package(version='1.0p1', flags=PackageFlags.P_IS_PATCH),
-            spawn_package(version='1.0p1', flags=PackageFlags.P_IS_PATCH),
-            0
-        )
-
-    def test_flag_outdated(self) -> None:
-        self.assert_version_compare(
-            spawn_package(version='1.0'),
-            spawn_package(version='1.0'),
-            0
-        )
-        self.assert_version_compare(
-            spawn_package(version='1.0', flags=PackageFlags.SINK),
-            spawn_package(version='1.0'),
-            -1
-        )
-        self.assert_version_compare(
-            spawn_package(version='1.0'),
-            spawn_package(version='1.0', flags=PackageFlags.SINK),
-            1
-        )
-        self.assert_version_compare(
-            spawn_package(version='1.0', flags=PackageFlags.SINK),
-            spawn_package(version='1.0', flags=PackageFlags.SINK),
-            0
-        )
-
-    def test_flag_rolling(self) -> None:
-        self.assert_version_compare(
-            spawn_package(version='1.0'),
-            spawn_package(version='1.0'),
-            0
-        )
-        self.assert_version_compare(
-            spawn_package(version='1.0', flags=PackageFlags.ROLLING),
-            spawn_package(version='1.0'),
-            1
-        )
-        self.assert_version_compare(
-            spawn_package(version='1.0'),
-            spawn_package(version='1.0', flags=PackageFlags.ROLLING),
-            -1
-        )
-        self.assert_version_compare(
-            spawn_package(version='1.0', flags=PackageFlags.ROLLING),
-            spawn_package(version='1.0', flags=PackageFlags.ROLLING),
-            0
-        )
-
-    def test_hash(self) -> None:
-        self.assertEqual(
-            spawn_package(
-                name='foo',
-                version='1.0',
-                links=[
-                    (LinkType.UPSTREAM_HOMEPAGE, 'http://foo'),
-                    (LinkType.UPSTREAM_HOMEPAGE, 'http://bar'),
-                ]
-            ).get_classless_hash(),
-            spawn_package(
-                name='foo',
-                version='1.0',
-                links=[
-                    (LinkType.UPSTREAM_HOMEPAGE, 'http://foo'),
-                    (LinkType.UPSTREAM_HOMEPAGE, 'http://bar'),
-                ]
-            ).get_classless_hash()
-        )
-
-        self.assertNotEqual(
-            spawn_package(name='foo', version='1.0').get_classless_hash(),
-            spawn_package(name='foo', version='1.1').get_classless_hash(),
-        )
-        self.assertNotEqual(
-            spawn_package(name='foo', version='1.0').get_classless_hash(),
-            spawn_package(name='foO', version='1.0').get_classless_hash(),
-        )
+def assert_version_compare(a: Package, b: Package, res: int) -> None:
+    assert a.version_compare(b) == res
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_equality() -> None:
+    assert spawn_package() == spawn_package()
+    assert spawn_package(name='a') == spawn_package(name='a')
+    assert spawn_package(
+        links=[
+            (LinkType.UPSTREAM_HOMEPAGE, 'http://foo'),
+            (LinkType.UPSTREAM_HOMEPAGE, 'http://foo'),
+            (LinkType.UPSTREAM_HOMEPAGE, 'http://bar'),
+            (LinkType.UPSTREAM_HOMEPAGE, 'http://bar'),
+        ]
+    ) == spawn_package(
+        links=[
+            (LinkType.UPSTREAM_HOMEPAGE, 'http://foo'),
+            (LinkType.UPSTREAM_HOMEPAGE, 'http://bar'),
+        ]
+    )
+
+    assert spawn_package(name='a') != spawn_package(name='b')
+    assert spawn_package() != spawn_package(name='b')
+    assert spawn_package(name='a') != spawn_package()
+
+
+def test_flag_p_is_patch() -> None:
+    assert_version_compare(
+        spawn_package(version='1.0p1'), spawn_package(version='1.0p1'), 0
+    )
+    assert_version_compare(
+        spawn_package(version='1.0p1', flags=PackageFlags.P_IS_PATCH),
+        spawn_package(version='1.0p1'),
+        1,
+    )
+    assert_version_compare(
+        spawn_package(version='1.0p1'),
+        spawn_package(version='1.0p1', flags=PackageFlags.P_IS_PATCH),
+        -1,
+    )
+    assert_version_compare(
+        spawn_package(version='1.0p1', flags=PackageFlags.P_IS_PATCH),
+        spawn_package(version='1.0p1', flags=PackageFlags.P_IS_PATCH),
+        0,
+    )
+
+
+def test_flag_sink() -> None:
+    assert_version_compare(
+        spawn_package(version='1.0'), spawn_package(version='1.0'), 0
+    )
+    assert_version_compare(
+        spawn_package(version='1.0', flags=PackageFlags.SINK),
+        spawn_package(version='1.0'),
+        -1,
+    )
+    assert_version_compare(
+        spawn_package(version='1.0'),
+        spawn_package(version='1.0', flags=PackageFlags.SINK),
+        1,
+    )
+    assert_version_compare(
+        spawn_package(version='1.0', flags=PackageFlags.SINK),
+        spawn_package(version='1.0', flags=PackageFlags.SINK),
+        0,
+    )
+
+
+def test_flag_rolling() -> None:
+    assert_version_compare(
+        spawn_package(version='1.0'), spawn_package(version='1.0'), 0
+    )
+    assert_version_compare(
+        spawn_package(version='1.0', flags=PackageFlags.ROLLING),
+        spawn_package(version='1.0'),
+        1,
+    )
+    assert_version_compare(
+        spawn_package(version='1.0'),
+        spawn_package(version='1.0', flags=PackageFlags.ROLLING),
+        -1,
+    )
+    assert_version_compare(
+        spawn_package(version='1.0', flags=PackageFlags.ROLLING),
+        spawn_package(version='1.0', flags=PackageFlags.ROLLING),
+        0,
+    )
+
+
+def test_hash() -> None:
+    assert (
+        spawn_package(
+            name='foo',
+            version='1.0',
+            links=[
+                (LinkType.UPSTREAM_HOMEPAGE, 'http://foo'),
+                (LinkType.UPSTREAM_HOMEPAGE, 'http://bar'),
+            ],
+        ).get_classless_hash()
+        == spawn_package(
+            name='foo',
+            version='1.0',
+            links=[
+                (LinkType.UPSTREAM_HOMEPAGE, 'http://foo'),
+                (LinkType.UPSTREAM_HOMEPAGE, 'http://bar'),
+            ],
+        ).get_classless_hash()
+    )
+
+    assert (
+        spawn_package(name='foo', version='1.0').get_classless_hash()
+        != spawn_package(name='foo', version='1.1').get_classless_hash()
+    )
+    assert (
+        spawn_package(name='foo', version='1.0').get_classless_hash()
+        != spawn_package(name='foO', version='1.0').get_classless_hash()
+    )
