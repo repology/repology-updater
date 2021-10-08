bb3d05c8088cc4f1b9824e10223213822625648d
Dmitry Marakasov <amdmi3@amdmi3.ru>
2021-10-08 18:31:59 +0300

    Add regression test for nix parser

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -26,6 +26,10 @@ def test_parsers_regress(regtest):
     repomgr = RepositoryManager(config['REPOS_DIR'])
     repoproc = RepositoryProcessor(repomgr, 'testdata', 'testdata', safety_checks=False)
 
+    # NOTE: run the following command to canonize this test after parser or testdata update
+    #
+    #     pytest -k test_parsers_regress --regtest-reset to canonize
+    #
     with regtest:
         for package in repoproc.iter_parse(reponames=['have_testdata']):
             print(json.dumps(package.__dict__, indent=1))
