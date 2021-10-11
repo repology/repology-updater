repology/repology-updater

commit b1b9359341520f7d5464c48133433b4cc35b8c07

Further refactor PackageTransformer
Split into separate classes:
- Ruleset, which is a representation of loaded ruleset
- RulesetIterator, which is an optimized view into ruleset usable to process packages

repology-update.py
@@ -31,6 +31,7 @@ from repology.querymgr import QueryManager
 from repology.repomgr import RepositoryManager
 from repology.repoproc import RepositoryProcessor
 from repology.transformer import PackageTransformer
+from repology.transformer.ruleset import Ruleset
 from repology.update import UpdateProcess
 
 
@@ -78,8 +79,8 @@ class Environment:
         return RepositoryProcessor(self.get_repo_manager(), self.options.statedir, self.options.parseddir, safety_checks=self.options.enable_safety_checks)
 
     @cached_method
-    def get_package_transformer(self) -> PackageTransformer:
-        return PackageTransformer(self.get_repo_manager(), self.options.rules_dir)
+    def get_ruleset(self) -> Ruleset:
+        return Ruleset(self.options.rules_dir)
 
     @cached_method
     def get_enabled_repo_names(self) -> list[str]:
@@ -142,9 +143,9 @@ def process_repositories(env: Environment) -> None:
             database.commit()
 
         if env.get_options().parse:
-            transformer = env.get_package_transformer()
+            ruleset = env.get_ruleset()
 
-            ruleset_hash_changed = transformer.get_ruleset_hash() != database.get_repository_ruleset_hash(reponame)
+            ruleset_hash_changed = ruleset.get_hash() != database.get_repository_ruleset_hash(reponame)
 
             if ruleset_hash_changed:
                 env.get_main_logger().log('parsing {}'.format(reponame))
@@ -159,6 +160,8 @@ def process_repositories(env: Environment) -> None:
             database.commit()
 
             try:
+                transformer = PackageTransformer(env.get_repo_manager(), ruleset)
+
                 with LogRunManager(env.get_logging_database_connection(), reponame, 'parse') as runlogger:
                     env.get_repo_processor().parse([reponame], transformer=transformer, logger=runlogger)
 
@@ -170,7 +173,7 @@ def process_repositories(env: Environment) -> None:
                 if env.get_options().fatal:
                     raise
 
-            database.update_repository_ruleset_hash(reponame, transformer.get_ruleset_hash())
+            database.update_repository_ruleset_hash(reponame, ruleset.get_hash())
             database.mark_repository_parsed(reponame)
             database.commit()
 
@@ -280,16 +283,6 @@ def database_update_post(env: Environment) -> None:
     database.commit()
 
 
-def dump_rules(env: Environment) -> None:
-    statistics = env.get_package_transformer().get_statistics()
-
-    for block in statistics.blocks:
-        print('{')
-        for rule, checks, matches in block:
-            print('    {:7} {:7}  {}'.format(checks, matches, (rule[:80] + '...') if len(rule) > 80 else rule))
-        print('}')
-
-
 def parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-S', '--statedir', default=config['STATE_DIR'], help='path to directory with repository state')
@@ -317,7 +310,6 @@ def parse_arguments() -> argparse.Namespace:
 
     grp = parser.add_argument_group('Informational queries')
     grp.add_argument('-l', '--list', action='store_true', help='list repositories repology will work on')
-    grp.add_argument('-r', '--dump-rules', action='store_true', help='dump rule statistics')
 
     grp = parser.add_argument_group('Flags')
     grp.add_argument('--enable-safety-checks', action='store_true', dest='enable_safety_checks', default=config['ENABLE_SAFETY_CHECKS'], help='enable safety checks on processed repository data')
@@ -363,9 +355,6 @@ def main() -> int:
 
     if options.postupdate:
         database_update_post(env)
-
-    if options.dump_rules:
-        dump_rules(env)
 
     if options.check_totals or options.fix_totals:
         handle_totals(env, options.fix_totals)

repology/transformer/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016-2018 Dmitry Marakasov <amdmi3@amdmi3.ru>
+# Copyright (C) 2016-2019,2021 Dmitry Marakasov <amdmi3@amdmi3.ru>
 #
 # This file is part of repology
 #
@@ -15,158 +15,25 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-import hashlib
-import os
 import sys
-from copy import deepcopy
-from typing import Any, Iterable, Optional
-
-import yaml
 
 from repology.package import Package, PackageFlags
 from repology.repomgr import RepositoryManager
-from repology.transformer.blocks import CoveringRuleBlock, NameMapRuleBlock, RuleBlock, SingleRuleBlock
 from repology.transformer.contexts import PackageContext
-from repology.transformer.rule import Rule
-
-
-RULE_LOWFREQ_THRESHOLD = 0.001  # best of 0.1, 0.01, 0.001, 0.0001
-COVERING_BLOCK_MIN_SIZE = 2  # covering block over single block impose extra overhead
-NAMEMAP_BLOCK_MIN_SIZE = 1  # XXX: test > 1 after rule optimizations
-SPLIT_MULTI_NAME_RULES = True
-
-
-class RulesetStatistics:
-    RuleStatistics = tuple[str, int, int]
-
-    blocks: list[list[RuleStatistics]] = []
-
-    def __init__(self) -> None:
-        self.blocks = []
-
-    def begin_block(self) -> None:
-        self.blocks.append([])
-
-    def add_rule(self, rule: Rule) -> None:
-        self.blocks[-1].append((rule.pretty, rule.checks, rule.matches))
-
-    def end_block(self) -> None:
-        pass
+from repology.transformer.iterator import RulesetIterator
+from repology.transformer.ruleset import Ruleset
 
 
 class PackageTransformer:
-    _repomgr: RepositoryManager
-    _rules: list[Rule]
-    _hash: str
-    _ruleblocks: list[RuleBlock]
-    _optruleblocks: list[RuleBlock]
-    _packages_processed: int
+    _ruleset: Ruleset
+    _iterator: RulesetIterator
 
-    def __init__(self, repomgr: RepositoryManager, rulesdir: Optional[str] = None, rulestext: Optional[str] = None):
+    def __init__(self, repomgr: RepositoryManager, ruleset: Ruleset) -> None:
         self._repomgr = repomgr
-        self._rules = []
-
-        hasher = hashlib.sha256()
-
-        if isinstance(rulestext, str):
-            hasher.update(rulestext.encode('utf-8'))
-            for ruledata in yaml.safe_load(rulestext):
-                self._add_rule(ruledata)
-        elif isinstance(rulesdir, str):
-            rulefiles: list[str] = []
-
-            for root, dirs, files in os.walk(rulesdir):
-                rulefiles += [os.path.join(root, f) for f in files if f.endswith('.yaml')]
-                dirs[:] = [d for d in dirs if not d.startswith('.')]
-
-            for rulefile in sorted(rulefiles):
-                with open(rulefile) as data:
-                    ruleset_text = data.read()
-                    hasher.update(ruleset_text.encode('utf-8'))
-
-                    rules = yaml.safe_load(ruleset_text)
-                    if rules:  # may be None for empty file
-                        for rule in rules:
-                            self._add_rule(rule)
-        else:
-            raise RuntimeError('rulesdir or rulestext must be defined')
-
-        self._hash = hasher.hexdigest()
-
-        self._ruleblocks = []
-
-        current_name_rules: list[Rule] = []
-
-        def flush_current_name_rules() -> None:
-            nonlocal current_name_rules
-            if len(current_name_rules) >= NAMEMAP_BLOCK_MIN_SIZE:
-                self._ruleblocks.append(NameMapRuleBlock(current_name_rules))
-            elif current_name_rules:
-                self._ruleblocks.extend([SingleRuleBlock(rule) for rule in current_name_rules])
-            current_name_rules = []
-
-        for rule in self._rules:
-            if rule.names:
-                current_name_rules.append(rule)
-            else:
-                flush_current_name_rules()
-                self._ruleblocks.append(SingleRuleBlock(rule))
-
-        flush_current_name_rules()
-
-        self._optruleblocks = self._ruleblocks
-        self._packages_processed = 0
-
-    def _add_rule(self, ruledata: dict[str, Any]) -> None:
-        if SPLIT_MULTI_NAME_RULES and 'name' in ruledata and isinstance(ruledata['name'], list):
-            for name in ruledata['name']:
-                modified_ruledata = deepcopy(ruledata)
-                modified_ruledata['name'] = name
-                self._rules.append(Rule(len(self._rules), modified_ruledata))
-        else:
-            self._rules.append(Rule(len(self._rules), ruledata))
-
-    def _recalc_opt_ruleblocks(self) -> None:
-        self._optruleblocks = []
-
-        current_lowfreq_blocks: list[RuleBlock] = []
-
-        def flush_current_lowfreq_blocks() -> None:
-            nonlocal current_lowfreq_blocks
-            if len(current_lowfreq_blocks) >= COVERING_BLOCK_MIN_SIZE:
-                self._optruleblocks.append(CoveringRuleBlock(current_lowfreq_blocks))
-            elif current_lowfreq_blocks:
-                self._optruleblocks.extend(current_lowfreq_blocks)
-            current_lowfreq_blocks = []
-
-        for block in self._ruleblocks:
-            max_matches = 0
-            has_unconditional = False
-            for rule in block.iter_all_rules():
-                max_matches = max(max_matches, rule.matches)
-                if not rule.names and not rule.namepat:
-                    has_unconditional = True
-                    break
-
-            if has_unconditional or max_matches >= self._packages_processed * RULE_LOWFREQ_THRESHOLD:
-                flush_current_lowfreq_blocks()
-                self._optruleblocks.append(block)
-                continue
-
-            current_lowfreq_blocks.append(block)
-
-        flush_current_lowfreq_blocks()
-
-    def _iter_package_rules(self, package: Package) -> Iterable[Rule]:
-        for ruleblock in self._optruleblocks:
-            yield from ruleblock.iter_rules(package)
+        self._ruleset = ruleset
+        self._iterator = RulesetIterator(ruleset)
 
     def process(self, package: Package) -> None:
-        self._packages_processed += 1
-
-        if self._packages_processed == 1000 or self._packages_processed == 10000 or self._packages_processed == 100000 or self._packages_processed == 1000000:
-            self._recalc_opt_ruleblocks()
-
         # XXX: duplicate code: PackageMaker does the same
         package.effname = package.projectname_seed
 
@@ -174,7 +41,7 @@ class PackageTransformer:
         if package.repo:
             package_context.set_rulesets(self._repomgr.get_repository(package.repo)['ruleset'])
 
-        for rule in self._iter_package_rules(package):
+        for rule in self._iterator.iter_rules_for_package(package):
             match_context = rule.match(package, package_context)
             if not match_context:
                 continue
@@ -189,18 +56,4 @@ class PackageTransformer:
         if package.has_flag(PackageFlags.TRACE):
             print('Rule trace for {} ({}) {} in {}'.format(package.effname, package.trackname or '???', package.version, package.repo), file=sys.stderr)
             for rulenum in package_context.matched_rules:
-                print('{:5d} {}'.format(rulenum, self._rules[rulenum].pretty), file=sys.stderr)
-
-    def get_statistics(self) -> RulesetStatistics:
-        statistics = RulesetStatistics()
-
-        for block in self._optruleblocks:
-            statistics.begin_block()
-            for rule in block.iter_all_rules():
-                statistics.add_rule(rule)
-            statistics.end_block()
-
-        return statistics
-
-    def get_ruleset_hash(self) -> str:
-        return self._hash
+                print('{:5d} {}'.format(rulenum, self._ruleset.get_rules()[rulenum].pretty), file=sys.stderr)

repology/transformer/iterator.py
@@ -1,0 +1,101 @@
+# Copyright (C) 2016-2019,2021 Dmitry Marakasov <amdmi3@amdmi3.ru>
+#
+# This file is part of repology
+#
+# repology is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# repology is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with repology.  If not, see <http://www.gnu.org/licenses/>.
+
+from typing import Iterator
+
+from repology.package import Package
+from repology.transformer.blocks import CoveringRuleBlock, NameMapRuleBlock, RuleBlock, SingleRuleBlock
+from repology.transformer.rule import Rule
+from repology.transformer.ruleset import Ruleset
+
+
+RULE_LOWFREQ_THRESHOLD = 0.001  # best of 0.1, 0.01, 0.001, 0.0001
+COVERING_BLOCK_MIN_SIZE = 2  # covering block over single block impose extra overhead
+NAMEMAP_BLOCK_MIN_SIZE = 1  # XXX: test > 1 after rule optimizations
+
+
+class RulesetIterator:
+    _ruleset: Ruleset
+    _ruleblocks: list[RuleBlock]
+    _optruleblocks: list[RuleBlock]
+    _packages_processed: int
+
+    def __init__(self, ruleset: Ruleset) -> None:
+        self._ruleset = ruleset
+        self._ruleblocks = []
+
+        current_name_rules: list[Rule] = []
+
+        def flush_current_name_rules() -> None:
+            nonlocal current_name_rules
+            if len(current_name_rules) >= NAMEMAP_BLOCK_MIN_SIZE:
+                self._ruleblocks.append(NameMapRuleBlock(current_name_rules))
+            elif current_name_rules:
+                self._ruleblocks.extend([SingleRuleBlock(rule) for rule in current_name_rules])
+            current_name_rules = []
+
+        for rule in self._ruleset.get_rules():
+            if rule.names:
+                current_name_rules.append(rule)
+            else:
+                flush_current_name_rules()
+                self._ruleblocks.append(SingleRuleBlock(rule))
+
+        flush_current_name_rules()
+
+        self._optruleblocks = self._ruleblocks
+        self._packages_processed = 0
+
+    def _recalc_opt_ruleblocks(self) -> None:
+        self._optruleblocks = []
+
+        current_lowfreq_blocks: list[RuleBlock] = []
+
+        def flush_current_lowfreq_blocks() -> None:
+            nonlocal current_lowfreq_blocks
+            if len(current_lowfreq_blocks) >= COVERING_BLOCK_MIN_SIZE:
+                self._optruleblocks.append(CoveringRuleBlock(current_lowfreq_blocks))
+            elif current_lowfreq_blocks:
+                self._optruleblocks.extend(current_lowfreq_blocks)
+            current_lowfreq_blocks = []
+
+        for block in self._ruleblocks:
+            max_matches = 0
+            has_unconditional = False
+            for rule in block.iter_all_rules():
+                max_matches = max(max_matches, rule.matches)
+                if not rule.names and not rule.namepat:
+                    has_unconditional = True
+                    break
+
+            if has_unconditional or max_matches >= self._packages_processed * RULE_LOWFREQ_THRESHOLD:
+                flush_current_lowfreq_blocks()
+                self._optruleblocks.append(block)
+                continue
+
+            current_lowfreq_blocks.append(block)
+
+        flush_current_lowfreq_blocks()
+
+    def iter_rules_for_package(self, package: Package) -> Iterator[Rule]:
+        self._packages_processed += 1
+
+        if self._packages_processed == 1000 or self._packages_processed == 10000 or self._packages_processed == 100000 or self._packages_processed == 1000000:
+            self._recalc_opt_ruleblocks()
+
+        for ruleblock in self._optruleblocks:
+            yield from ruleblock.iter_rules(package)

commit b77ededa095de02d68e141762c67ff7a99be41d2

Store rule match statistics in a separate object
- Make Ruleset completely immutable, and introduce dedicated
  RuleMatchStatistics object to hold statistics on how many times a
  rule matches.
- Switch Transformer onto it, with ability to store statistics for
  repository and reuse it on the next run (persistence is not used
  yet).
- Rule match statistics are not calculated correctly, as before
  rule matches were calculated for a whole updater run, while number
  of packages processed was calculated per-repository.

Thil yields another ~2x parse performance gain and completely fixes
performance regression introduced in b1b9359 as statistics are now
gathered in a correct way. Overall, there is ~40% parse performance
gain compared to 674db9c.

repology-update.py
@@ -166,6 +166,8 @@ def process_repositories(env: Environment) -> None:
                     env.get_repo_processor().parse([reponame], transformer=transformer, logger=runlogger)
 
                 env.get_main_logger().get_indented().log('done')
+
+                transformer.finalize()
             except KeyboardInterrupt:
                 raise
             except Exception as e:

repology/transformer/__init__.py
@@ -22,17 +22,26 @@ from repology.package import Package, PackageFlags
 from repology.transformer.contexts import PackageContext
 from repology.transformer.iterator import RulesetIterator
 from repology.transformer.ruleset import Ruleset
+from repology.transformer.statistics import RuleMatchStatistics
 
 
 class PackageTransformer:
     _ruleset: Ruleset
     _repository_name: str
+    _active_statistics: RuleMatchStatistics
+    _next_statistics: RuleMatchStatistics
     _iterator: RulesetIterator
 
+    # XXX: introduce a dataclass in RepoMgr to hold repository information and pass it here
+    # instead of repository_name and rulesets. We should also get path to persistent rule match
+    # statistics file from it.
     def __init__(self, ruleset: Ruleset, repository_name: str, rulesets: Iterable[str]) -> None:
         self._ruleset = ruleset
         self._repository_name = repository_name
-        self._iterator = RulesetIterator(ruleset, set(rulesets))
+        self._active_statistics = RuleMatchStatistics()  # XXX: load persistent statistics here
+        self._next_statistics = RuleMatchStatistics()
+
+        self._iterator = RulesetIterator(ruleset, set(rulesets), self._active_statistics)
 
     def process(self, package: Package) -> None:
         # XXX: duplicate code: PackageMaker does the same
@@ -43,10 +52,23 @@ class PackageTransformer:
         if package.repo != self._repository_name:
             raise RuntimeError(f'not expected package from repository "{package.repo}" with ruleset for repository "{self._repository_name}"')
 
+        if self._active_statistics is self._next_statistics:
+            if self._active_statistics.get_total_packages() in (10, 100, 1000, 10000, 100000):
+                self._iterator.update_statistics(self._active_statistics)
+        else:
+            if self._next_statistics.get_total_packages() > self._active_statistics.get_total_packages():
+                self._active_statistics = self._next_statistics
+                self._iterator.update_statistics(self._active_statistics)
+
+        self._next_statistics.count_package()
+
         for rule in self._iterator.iter_rules_for_package(package):
             match_context = rule.match(package, package_context)
             if not match_context:
                 continue
+
+            self._next_statistics.count_rule_match(rule.texthash)
+
             rule.apply(package, package_context, match_context)
             if match_context.last:
                 break
@@ -59,3 +81,6 @@ class PackageTransformer:
             print('Rule trace for {} ({}) {} in {}'.format(package.effname, package.trackname or '???', package.version, package.repo), file=sys.stderr)
             for rulenum in package_context.matched_rules:
                 print('{:5d} {}'.format(rulenum, self._ruleset.get_rules()[rulenum].pretty), file=sys.stderr)
+
+    def finalize(self) -> None:
+        pass  # XXX: save _next_statistics here

repology/transformer/iterator.py
@@ -21,6 +21,7 @@ from repology.package import Package
 from repology.transformer.blocks import CoveringRuleBlock, NameMapRuleBlock, RuleBlock, SingleRuleBlock
 from repology.transformer.rule import Rule
 from repology.transformer.ruleset import Ruleset
+from repology.transformer.statistics import RuleMatchStatistics
 
 
 RULE_LOWFREQ_THRESHOLD = 0.001  # best of 0.1, 0.01, 0.001, 0.0001
@@ -31,9 +32,10 @@ NAMEMAP_BLOCK_MIN_SIZE = 1  # XXX: test > 1 after rule optimizations
 class RulesetIterator:
     _ruleblocks: list[RuleBlock]
     _optruleblocks: list[RuleBlock]
-    _packages_processed: int
 
-    def __init__(self, ruleset: Ruleset, rulesets: set[str]) -> None:
+    _statistics: RuleMatchStatistics
+
+    def __init__(self, ruleset: Ruleset, rulesets: set[str], statistics: RuleMatchStatistics) -> None:
         self._ruleblocks = []
 
         current_name_rules: list[Rule] = []
@@ -61,7 +63,9 @@ class RulesetIterator:
         flush_current_name_rules()
 
         self._optruleblocks = self._ruleblocks
-        self._packages_processed = 0
+
+        self._statistics = statistics
+        self._recalc_opt_ruleblocks()
 
     def _recalc_opt_ruleblocks(self) -> None:
         self._optruleblocks = []
@@ -77,15 +81,15 @@ class RulesetIterator:
             current_lowfreq_blocks = []
 
         for block in self._ruleblocks:
-            max_matches = 0
+            max_frequency = 0.0
             has_unconditional = False
             for rule in block.iter_all_rules():
-                max_matches = max(max_matches, rule.matches)
+                max_frequency = max(max_frequency, self._statistics.get_rule_frequency(rule.texthash))
                 if not rule.names and not rule.namepat:
                     has_unconditional = True
                     break
 
-            if has_unconditional or max_matches >= self._packages_processed * RULE_LOWFREQ_THRESHOLD:
+            if has_unconditional or max_frequency >= RULE_LOWFREQ_THRESHOLD:
                 flush_current_lowfreq_blocks()
                 self._optruleblocks.append(block)
                 continue
@@ -94,11 +98,10 @@ class RulesetIterator:
 
         flush_current_lowfreq_blocks()
 
+    def update_statistics(self, statistics: RuleMatchStatistics) -> None:
+        self._statistics = statistics
+        self._recalc_opt_ruleblocks()
+
     def iter_rules_for_package(self, package: Package) -> Iterator[Rule]:
-        self._packages_processed += 1
-
-        if self._packages_processed == 1000 or self._packages_processed == 10000 or self._packages_processed == 100000 or self._packages_processed == 1000000:
-            self._recalc_opt_ruleblocks()
-
         for ruleblock in self._optruleblocks:
             yield from ruleblock.iter_rules(package)

repology/transformer/rule.py
@@ -17,6 +17,8 @@
 
 from typing import Any, Callable
 
+import xxhash
+
 from repology.package import Package
 from repology.transformer.actions import get_action_generators
 from repology.transformer.contexts import MatchContext, PackageContext
@@ -25,7 +27,7 @@ from repology.transformer.util import DOLLAR0, yaml_as_list, yaml_as_set
 
 
 class Rule:
-    __slots__ = ['_matchers', '_actions', 'names', 'namepat', 'rulesets', 'norulesets', 'checks', 'matches', 'number', 'pretty']
+    __slots__ = ['_matchers', '_actions', 'names', 'namepat', 'rulesets', 'norulesets', 'number', 'pretty', 'texthash']
 
     _matchers: list[Callable[[Package, PackageContext, MatchContext], bool]]
     _actions: list[Callable[[Package, PackageContext, MatchContext], None]]
@@ -33,10 +35,9 @@ class Rule:
     namepat: str | None
     rulesets: set[str] | None
     norulesets: set[str] | None
-    checks: int
-    matches: int
     number: int
     pretty: str
+    texthash: int
 
     def __init__(self, number: int, ruledata: dict[str, Any]) -> None:
         self.names = None
@@ -44,10 +45,9 @@ class Rule:
         self.rulesets = None
         self.norulesets = None
         self.number = number
-        self.checks = 0
-        self.matches = 0
 
         self.pretty = str(ruledata)
+        self.texthash = xxhash.xxh64_intdigest(self.pretty)
 
         self._matchers = []
         self._actions = []
@@ -88,13 +88,10 @@ class Rule:
     def match(self, package: Package, package_context: PackageContext) -> MatchContext | None:
         match_context = MatchContext()
 
-        self.checks += 1
-
         for matcher in self._matchers:
             if not matcher(package, package_context, match_context):
                 return None
 
-        self.matches += 1
         package_context.add_matched_rule(self.number)
 
         return match_context

repology/transformer/statistics.py
@@ -1,0 +1,77 @@
+# Copyright (C) 2021 Dmitry Marakasov <amdmi3@amdmi3.ru>
+#
+# This file is part of repology
+#
+# repology is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# repology is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with repology.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import pickle
+from collections import defaultdict
+
+
+class RuleMatchStatistics:
+    """Registry of Rule match frequency
+
+    This class stores information in Rule match frequency, e.g. how
+    many times a Rule was matched per total number of Packages
+    processed. This information is used in optimized Ruleset lookups.
+
+    This class also supports persist storage, so complete frequency
+    info from a previous parse run may be reused for the next run.
+
+    Note that we cannot reference Rules by number here, as numbers
+    may change between parse runes, so Rules are instead referenced
+    by their hashes.
+    """
+    __slots__ = ['_total_packages', '_rule_match_counts']
+
+    _total_packages: int
+    _rule_match_counts: dict[int, int]
+
+    def __init__(self, path: str | None = None) -> None:
+        self._total_packages = 0
+        self._rule_match_counts = defaultdict(int)
+
+        if path is not None:
+            self.load(path)
+
+    def count_package(self) -> None:
+        self._total_packages += 1
+
+    def count_rule_match(self, rulehash: int) -> None:
+        self._rule_match_counts[rulehash] += 1
+
+    def get_rule_frequency(self, rulehash: int) -> float:
+        if self._total_packages == 0:
+            return 0.0
+
+        return self._rule_match_counts.get(rulehash, 0.0) / self._total_packages
+
+    def get_total_packages(self) -> int:
+        return self._total_packages
+
+    def load(self, path: str) -> None:
+        try:
+            with open(path, 'rb') as fd:
+                loaded = pickle.load(fd)
+                self._total_packages = loaded._total_packages
+                self._rule_match_counts = loaded._rule_match_counts
+        except (EOFError, FileNotFoundError, pickle.UnpicklingError):
+            pass
+
+    def dump(self, path: str) -> None:
+        with open(path, 'wb') as fd:
+            pickle.dump(self, fd)
+            fd.flush()
+            os.fsync(fd.fileno())