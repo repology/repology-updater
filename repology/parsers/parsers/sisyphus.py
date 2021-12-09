02d28d5b39a8672328f1ab3f6caef79977034943
Danil Shein <dshein@altlinux.org>
2021-12-09 08:48:18 +0300

    PR fix-up
    - moved all package's links from YAML to JSON parser
    - added GPL header
    - removed unnecessary extra fields
    - fixed flake8 error

--- repology/parsers/parsers/sisyphus.py
+++ repology/parsers/parsers/sisyphus.py
@@ -1,6 +1,23 @@
+# Copyright (C) 2021 Danil Shein <dshein@altlinux.org>
+#
+# This file is part of repology
+#
+# repology is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# repology is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with repology.  If not, see <http://www.gnu.org/licenses/>.
+
 from typing import Iterable
 
-from repology.packagemaker import PackageFactory, PackageMaker, LinkType
+from repology.packagemaker import NameType, LinkType, PackageFactory, PackageMaker
 from repology.parsers import Parser
 from repology.parsers.json import iter_json_list
 
@@ -9,7 +26,7 @@ class SisyphusJsonParser(Parser):
     def iter_parse(self, path: str, factory: PackageFactory) -> Iterable[PackageMaker]:
         for packagedata in iter_json_list(path, ('packages', None)):
             with factory.begin() as pkg:
-                pkg.add_name(packagedata['name'])
+                pkg.add_name(packagedata['name'], NameType.GENERIC_GEN_NAME)
                 pkg.set_version(packagedata['version'])
                 pkg.set_rawversion(
                     f"{packagedata['epoch']}:{packagedata['version']}-{packagedata['release']}"
@@ -19,11 +36,7 @@ class SisyphusJsonParser(Parser):
                 pkg.set_summary(packagedata['summary'])
                 pkg.add_licenses(packagedata['license'])
                 pkg.add_maintainers(packagedata['packager'])
-                # store package epoch and release
-                pkg.set_extra_field('epoch', packagedata['epoch'])
-                pkg.set_extra_field('release', packagedata['release'])
                 # store source package binaries
-                # XXX: is there any way to store binary packages extra fields?
                 pkg.add_binnames(
                     [b['name'] for b in packagedata['binaries']]
                 )
@@ -31,6 +44,10 @@ class SisyphusJsonParser(Parser):
                 pkg.add_links(LinkType.PACKAGE_HOMEPAGE, packagedata['homepage'])
                 pkg.add_links(LinkType.PACKAGE_RECIPE_RAW, packagedata['recipe'])
                 pkg.add_links(LinkType.PACKAGE_ISSUE_TRACKER, packagedata['bugzilla'])
+                pkg.add_links(
+                    LinkType.PACKAGE_RECIPE,
+                    packagedata['recipe'].rsplit('/', maxsplit=1)
+                )
                 # TODO: parse CPE data when available
                 if 'CPE' in packagedata:
                     pass
