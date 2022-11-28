6299aeb52ee7f0d662cde429705f00aec936b34d
Dmitry Marakasov <amdmi3@amdmi3.ru>
2022-11-28 22:40:56 +0300

    Remove unused import

--- repology/fetchers/fetchers/file.py
+++ repology/fetchers/fetchers/file.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import time
 
 from repology.atomic_fs import AtomicFile
