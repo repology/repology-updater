e63abd7984c8e74388dcaa1b04c49068a695c699
Dmitry Marakasov <amdmi3@amdmi3.ru>
2023-03-27 22:10:20 +0300

    Modernize winget parser
    - Implement parsing data from all manifest files. This allows to
      parse most info from e.g. foo.locale.en-us.yaml, and download
      urls from foo.installer.yaml
    - Only parse default locales, removing chinese package duplicates
    - Parse more links, including upstream documentation and changelogs
    - Don't generate recipe links, as manifest files are not really
      recipes, and there are multiple of them, so it's not quite
      correct to point to a specific one

--- repology/parsers/parsers/winget.py
+++ repology/parsers/parsers/winget.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-2021 Dmitry Marakasov <amdmi3@amdmi3.ru>
+# Copyright (C) 2020-2021, 2023 Dmitry Marakasov <amdmi3@amdmi3.ru>
 #
 # This file is part of repology
 #
@@ -16,83 +16,103 @@
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
 import os
-from dataclasses import dataclass
-from typing import Iterable
+from typing import Any, Iterable
 
 import yaml
 
 from repology.logger import Logger
+from repology.package import LinkType
 from repology.packagemaker import NameType, PackageFactory, PackageMaker
 from repology.parsers import Parser
 from repology.parsers.walk import walk_tree
 
 
-@dataclass
-class _PackageLocation:
-    yamlpath_abs: str
-    yamlpath_rel: str
-    relevant_path: str
+_DOCUMENT_LABEL_TO_LINK_TYPE = {
+    '': LinkType.UPSTREAM_DOCUMENTATION,
+    'docs': LinkType.UPSTREAM_DOCUMENTATION,
+    'documentation': LinkType.UPSTREAM_DOCUMENTATION,
+    'guide': LinkType.UPSTREAM_DOCUMENTATION,
+    'manual': LinkType.UPSTREAM_DOCUMENTATION,
+    'user guide': LinkType.UPSTREAM_DOCUMENTATION,
+    'wiki': LinkType.UPSTREAM_WIKI,
+    # a lot of 'FAQ' as well, is it worth adding?
+}
 
 
-def _iter_packages(path: str) -> Iterable[_PackageLocation]:
-    for yamlpath_abs in walk_tree(os.path.join(path, 'manifests'), suffix='.yaml'):
-        yamlpath_rel = os.path.relpath(yamlpath_abs, path)
+def _iter_directories(path: str) -> Iterable[str]:
+    prev_dir: str | None = None
 
-        yield _PackageLocation(
-            yamlpath_abs=yamlpath_abs,
-            yamlpath_rel=yamlpath_rel,
-            # skip manifests/ at left
-            # skip version directory and yaml filename at right
-            relevant_path='/'.join(yamlpath_rel.split('/')[1:-2]),
-        )
+    for yamlpath in walk_tree(os.path.join(path, 'manifests'), suffix='.yaml'):
+        cur_dir = os.path.dirname(yamlpath)
+
+        if cur_dir != prev_dir:
+            yield cur_dir
+            prev_dir = cur_dir
+
+
+def _parse_manifest(manifest_data: dict[str, Any], pkg: PackageMaker) -> None:
+    if manifest_data['ManifestType'] not in ['singleton', 'installer', 'version', 'locale', 'defaultLocale']:
+        pkg.log(f'Unknown ManifestType {manifest_data["ManifestType"]}', Logger.ERROR)
+
+    if manifest_data['ManifestType'] in ['singleton', 'installer']:
+        for installer in manifest_data['Installers']:
+            pkg.add_links(LinkType.UPSTREAM_DOWNLOAD, installer['InstallerUrl'])
+
+    if manifest_data['ManifestType'] in ['singleton', 'defaultLocale']:
+        pkg.add_name(manifest_data['PackageIdentifier'], NameType.WINGET_ID)
+        pkg.add_name(manifest_data['PackageIdentifier'].split('.', 1)[-1], NameType.WINGET_ID_NAME)
+        pkg.add_name(manifest_data['PackageName'], NameType.WINGET_NAME)
+        # Moniker field is optional and mostly useless
+
+        version = manifest_data['PackageVersion']
+        if isinstance(version, float):
+            pkg.log(f'PackageVersion "{version}" is a floating point, should be quoted in YAML', Logger.WARNING)
+
+        pkg.set_version(str(version))
+        pkg.add_links(LinkType.UPSTREAM_HOMEPAGE, manifest_data.get('PackageUrl'))
+        pkg.add_links(LinkType.UPSTREAM_CHANGELOG, manifest_data.get('ReleaseNotesUrl'))
+
+        for documentation in manifest_data.get('Documentations', []):
+            if link_type := _DOCUMENT_LABEL_TO_LINK_TYPE.get(documentation.get('DocumentLabel', '').lower()):
+                pkg.add_links(link_type, documentation['DocumentUrl'])
+
+        # pkg.set_summary(manifest_data.get('Description'))  # may be long
+        # pkg.add_licenses(manifest_data['License'])  # long garbage
+
+        pkg.add_categories(map(str, manifest_data.get('Tags', [])))
 
 
 class WingetGitParser(Parser):
     def iter_parse(self, path: str, factory: PackageFactory) -> Iterable[PackageMaker]:
-        num_manifests_without_packagename = 0
+        for pkgpath_abs in _iter_directories(path):
+            pkgpath_rel = os.path.relpath(pkgpath_abs, path)
 
-        for pkgloc in _iter_packages(path):
-            with factory.begin(pkgloc.yamlpath_rel) as pkg:
-                try:
-                    with open(pkgloc.yamlpath_abs, 'r') as fd:
-                        pkgdata = yaml.safe_load(fd)
-                except UnicodeDecodeError:
-                    pkg.log('Cannot read file, probably UTF-16 garbage', Logger.ERROR)
-                    continue
-                except yaml.MarkedYAMLError as e:
-                    if e.problem_mark:
-                        pkg.log(f'YAML error at line {e.problem_mark.line}: {e.problem}', Logger.ERROR)
-                    else:
-                        pkg.log(f'YAML error: {e.problem}', Logger.ERROR)
-                    continue
+            with factory.begin(pkgpath_rel) as pkg:
+                manifests = [filename for filename in os.listdir(pkgpath_abs) if filename.endswith('.yaml')]
 
-                if 'PackageName' not in pkgdata:
-                    num_manifests_without_packagename += 1
-                    continue
+                for manifest in manifests:
+                    try:
+                        with open(os.path.join(pkgpath_abs, manifest), 'r') as fd:
+                            manifest_data = yaml.safe_load(fd)
+                    except UnicodeDecodeError:
+                        pkg.log(f'failed to decode {manifest}, probably UTF-16 garbage', Logger.ERROR)
+                        continue
+                    except yaml.MarkedYAMLError as e:
+                        if e.problem_mark:
+                            pkg.log(f'YAML error in {manifest} at line {e.problem_mark.line}: {e.problem}', Logger.ERROR)
+                        else:
+                            pkg.log(f'YAML error in {manifest}: {e.problem}', Logger.ERROR)
+                        continue
 
-                pkg.add_name(pkgdata['PackageIdentifier'], NameType.WINGET_ID)
-                pkg.add_name(pkgdata['PackageIdentifier'].split('.', 1)[-1], NameType.WINGET_ID_NAME)
-                pkg.add_name(pkgdata['PackageName'], NameType.WINGET_NAME)
-                pkg.add_name(pkgloc.relevant_path, NameType.WINGET_PATH)
-                # Moniker field is optional and mostly useless
+                    _parse_manifest(manifest_data, pkg)
 
-                version = pkgdata['PackageVersion']
-                if isinstance(version, float):
-                    pkg.log(f'PackageVersion "{version}" is a floating point, should be quoted in YAML', Logger.WARNING)
+                # skip manifests/ at the left and version directory at the right
+                relevant_path = '/'.join(pkgpath_rel.split('/')[1:-1])
+                pkg.add_name(relevant_path, NameType.WINGET_PATH)
 
-                pkg.set_version(str(version))
-                pkg.add_homepages(pkgdata.get('PackageUrl'))
+                pkg.set_extra_field('path', pkgpath_rel)
 
-                # pkg.set_summary(pkgdata.get('Description'))  # may be long
-                # pkg.add_licenses(pkgdata['License'])  # long garbage
-
-                pkg.add_categories(map(str, pkgdata.get('Tags', [])))
-
-                if 'Installers' in pkgdata:
-                    pkg.add_downloads(installer['InstallerUrl'] for installer in pkgdata['Installers'])
-
-                pkg.set_extra_field('yamlpath', pkgloc.yamlpath_rel)
-
-                yield pkg
-
-        factory.log(f'Skipped manifests without PackageName: {num_manifests_without_packagename}', Logger.WARNING)
+                if not pkg.version:
+                    raise RuntimeError('could not parse required information from all manifests')
+                else:
+                    yield pkg
