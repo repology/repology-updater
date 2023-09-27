8e19da3e13bc03593fabe4d99208e687ffa9fc50
Dmitry Marakasov <amdmi3@amdmi3.ru>
2023-09-27 15:55:04 +0300

    crates.io parser: differentiate stable and devel versions

--- repology/parsers/parsers/cratesio.py
+++ repology/parsers/parsers/cratesio.py
@@ -19,33 +19,44 @@ import json
 import os
 from typing import Iterable
 
-from repology.package import LinkType
+from repology.package import LinkType, PackageFlags
 from repology.packagemaker import NameType, PackageFactory, PackageMaker
 from repology.parsers import Parser
 
 
 class CratesIOParser(Parser):
-    def iter_parse(self, path: str, factory: PackageFactory) -> Iterable[PackageMaker]:
-        for pagefilename in os.listdir(path):
-            if not pagefilename.endswith('.json'):
-                continue
-
-            pagepath = os.path.join(path, pagefilename)
-
-            with open(pagepath, 'r', encoding='utf-8', errors='ignore') as pagedata:
-                for crate in json.load(pagedata)['crates']:
-                    pkg = factory.begin()
-
+    def iter_parse_one_file(self, path: str, factory: PackageFactory) -> Iterable[PackageMaker]:
+        with open(path, 'r', encoding='utf-8', errors='ignore') as pagedata:
+            for crate in json.load(pagedata)['crates']:
+                with factory.begin(crate.get('id')) as pkg:
                     if crate['id'] != crate['name']:
                         raise RuntimeError('id != name')
 
                     pkg.add_name(crate['id'], NameType.CRATESIO_ID)
-                    pkg.set_version(crate['max_version'])
-
-                    pkg.set_summary(crate['description'])
 
                     pkg.add_links(LinkType.UPSTREAM_HOMEPAGE, crate['homepage'])
                     pkg.add_links(LinkType.UPSTREAM_REPOSITORY, crate['repository'])
                     pkg.add_links(LinkType.UPSTREAM_DOCUMENTATION, crate['documentation'])
 
-                    yield pkg
+                    pkg.set_summary(crate['description'])
+
+                    if crate['max_stable_version'] == crate['max_version']:
+                        versions_with_flags = [(crate['max_stable_version'], 0)]
+                    elif crate['max_stable_version'] is None:
+                        versions_with_flags = [(crate['max_version'], PackageFlags.DEVEL)]
+                    else:
+                        versions_with_flags = [
+                            (crate['max_stable_version'], 0),
+                            (crate['max_version'], PackageFlags.DEVEL)
+                        ]
+
+                    for version, flags in versions_with_flags:
+                        verpkg = pkg.clone()
+                        verpkg.set_version(version)
+                        verpkg.set_flags(flags)
+                        yield verpkg
+
+    def iter_parse(self, path: str, factory: PackageFactory) -> Iterable[PackageMaker]:
+        for pagefilename in os.listdir(path):
+            if pagefilename.endswith('.json'):
+                yield from self.iter_parse_one_file(os.path.join(path, pagefilename), factory)
