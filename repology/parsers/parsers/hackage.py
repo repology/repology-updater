7f3d8f91d5a1ad9a5fa68a220ebacfe479c8abe6
Dmitry Marakasov <amdmi3@amdmi3.ru>
2024-03-04 13:23:58 +0300

    Remove unused import

--- repology/parsers/parsers/hackage.py
+++ repology/parsers/parsers/hackage.py
@@ -15,7 +15,6 @@
 # You should have received a copy of the GNU General Public License
 # along with repology.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
 import re
 import tarfile
 from io import StringIO
