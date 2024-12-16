8dfc3bc45d75667edcadd65f27f9996b0813c203
Dmitry Marakasov <amdmi3@amdmi3.ru>
2024-12-16 23:10:39 +0300

    Make flat statistics history columns mandatory

--- sql.d/schema/create_schema_tables.sql
+++ sql.d/schema/create_schema_tables.sql
@@ -468,10 +468,10 @@ DROP TABLE IF EXISTS statistics_history CASCADE;
 CREATE TABLE statistics_history (
 	ts timestamp with time zone NOT NULL PRIMARY KEY,
 	snapshot jsonb NOT NULL,
-	num_packages integer,
-	num_projects integer,
-	num_problems integer,
-	num_maintainers integer
+	num_packages integer NOT NULL,
+	num_projects integer NOT NULL,
+	num_problems integer NOT NULL,
+	num_maintainers integer NOT NULL
 );
 
 --------------------------------------------------------------------------------
