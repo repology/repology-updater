80639b88622225b421540d6c7de64f39b87d26b8
Dmitry Marakasov <amdmi3@amdmi3.ru>
2025-04-30 22:54:30 +0300

    Remove links.ipv[46]_success columns, no longer used

--- sql.d/schema/create_schema_tables.sql
+++ sql.d/schema/create_schema_tables.sql
@@ -498,13 +498,11 @@ CREATE TABLE links (
 
 	ipv4_last_success timestamp with time zone,
 	ipv4_last_failure timestamp with time zone,
-	ipv4_success boolean,
 	ipv4_status_code smallint,
 	ipv4_permanent_redirect_target text,
 
 	ipv6_last_success timestamp with time zone,
 	ipv6_last_failure timestamp with time zone,
-	ipv6_success boolean,
 	ipv6_status_code smallint,
 	ipv6_permanent_redirect_target text
 );
