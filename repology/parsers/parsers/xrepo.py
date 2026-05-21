8a190d6d084cb38f08068ae17300cbf886d2232c
Dmitry Marakasov <amdmi3@amdmi3.ru>
2026-05-21 18:43:41 +0300

    xrepo: Refactor and simplify parser

--- repology/parsers/parsers/xrepo.py
+++ repology/parsers/parsers/xrepo.py
@@ -23,36 +23,19 @@ from repology.parsers import Parser
 from repology.parsers.json import iter_json_list
 
 
-def _normalize_version(version: str) -> str:
-    if version.startswith('v') and len(version) > 1 and version[1].isdigit():
-        return version[1:]
-    return version
-
-
 class XrepoJsonParser(Parser):
     def iter_parse(self, path: str, factory: PackageFactory) -> Iterable[PackageMaker]:
         for pkgdata in iter_json_list(path, ('packages', None)):
-            name = pkgdata.get('name')
-            version = pkgdata.get('version')
-            if not name or not version:
-                continue
-
-            with factory.begin(name) as pkg:
+            with factory.begin() as pkg:
+                name = pkgdata['name']
                 pkg.add_name(name, NameType.XREPO_NAME)
-                pkg.set_version(version, _normalize_version)
+                pkg.set_version(pkgdata['version'])
                 pkg.set_extra_field('xrepo_package_path', f'packages/{name[0]}/{name}')
 
-                if (summary := pkgdata.get('description')):
-                    pkg.set_summary(summary)
-                if (homepage := pkgdata.get('homepage')):
-                    pkg.add_links(LinkType.UPSTREAM_HOMEPAGE, homepage)
-                if (license_ := pkgdata.get('license')):
-                    pkg.add_licenses(license_)
-                if (repo_url := pkgdata.get('repository_url')):
-                    pkg.add_links(LinkType.UPSTREAM_REPOSITORY, repo_url)
-
-                download_url = pkgdata.get('download_url')
-                if download_url and not download_url.endswith('.git'):
-                    pkg.add_links(LinkType.UPSTREAM_DOWNLOAD, download_url)
+                pkg.set_summary(pkgdata.get('description'))
+                pkg.add_links(LinkType.UPSTREAM_HOMEPAGE, pkgdata.get('homepage'))
+                pkg.add_licenses(pkgdata.get('license'))
+                pkg.add_links(LinkType.UPSTREAM_REPOSITORY, pkgdata.get('repository_url'))
+                pkg.add_links(LinkType.UPSTREAM_DOWNLOAD, pkgdata.get('download_url'))
 
                 yield pkg
